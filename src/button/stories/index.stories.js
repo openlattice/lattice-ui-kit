124ea20e5fc31cc3acc08622ed9892eafa3a8fd7
Solomon Tang <solomoncztang@gmail.com>
2018-05-30 18:38:57 -0700

    button story

--- src/button/stories/index.stories.js
+++ src/button/stories/index.stories.js
@@ -4,8 +4,19 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Button from '../../button';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../theme/theme';
 
 storiesOf('Button', module)
   .add('with text', () => (
-    <Button onClick={action('button clicked')}>Hello Button</Button>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Button onClick={action('button clicked')}>Default</Button>
+        <Button primary onClick={action('button clicked')}>Primary</Button>
+        <Button secondary onClick={action('button clicked')}>Secondary</Button>
+        <Button warning onClick={action('button clicked')}>Warning</Button>
+        <Button error onClick={action('button clicked')}>Error</Button>
+        <Button disabled onClick={action('button clicked')}>Disabled</Button>
+      </div>
+    </ThemeProvider>
   ));
