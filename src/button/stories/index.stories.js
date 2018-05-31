5dd22b905cbede004d0f5d33a921ee7cc210c644
Solomon Tang <solomoncztang@gmail.com>
2018-05-30 19:07:58 -0700

    disabled style and story

--- src/button/stories/index.stories.js
+++ src/button/stories/index.stories.js
@@ -11,12 +11,19 @@ storiesOf('Button', module)
   .add('with text', () => (
     <ThemeProvider theme={theme}>
       <div>
+        <h1>Enabled</h1>
         <Button onClick={action('button clicked')}>Default</Button>
         <Button primary onClick={action('button clicked')}>Primary</Button>
         <Button secondary onClick={action('button clicked')}>Secondary</Button>
         <Button warning onClick={action('button clicked')}>Warning</Button>
         <Button error onClick={action('button clicked')}>Error</Button>
-        <Button disabled onClick={action('button clicked')}>Disabled</Button>
+        <hr />
+        <h1>Disabled</h1>
+        <Button disabled onClick={action('button clicked')}>Default</Button>
+        <Button primary disabled onClick={action('button clicked')}>Primary</Button>
+        <Button secondary disabled onClick={action('button clicked')}>Secondary</Button>
+        <Button warning disabled onClick={action('button clicked')}>Warning</Button>
+        <Button error disabled onClick={action('button clicked')}>Error</Button>
       </div>
     </ThemeProvider>
   ));
