11d38287c94c09db38575de06de88e82d462d598
Solomon Tang <solomoncztang@gmail.com>
2018-07-27 14:51:47 -0700

    lint

--- src/select/stories/index.stories.js
+++ src/select/stories/index.stories.js
@@ -8,34 +8,41 @@ import { stateOptions } from './storyConsts';
 storiesOf('Select', module)
   .add('Default Select', () => (
     <div>
-      <h1>Select</h1>
-      <a href="https://react-select.com/home">React-Select Documentation</a>
+      <h1>
+        Select
+      </h1>
+      <a href="https://react-select.com/home">
+        React-Select Documentation
+      </a>
       <Select
           options={stateOptions}
-          onChange={action('Single select changed')}
-          />
-      <h1>Multiple Select</h1>
+          onChange={action('Single select changed')} />
+      <h1>
+        Multiple Select
+      </h1>
       <Select
           options={stateOptions}
           onChange={action('Multiple selection changed')}
-          isMulti
-          />
+          isMulti />
     </div>
   ))
   .add('Creatable Select', () => (
     <div>
-      <h1>Creatable Select</h1>
-      <a href="https://react-select.com/home">React-Select Documentation</a>
+      <h1>
+        Creatable Select
+      </h1>
+      <a href="https://react-select.com/home">
+        React-Select Documentation
+      </a>
       <Creatable
           options={stateOptions}
-          onChange={action('Single creatable changed')}
-          />
-      <h1>Creatable Multiple Select</h1>
+          onChange={action('Single creatable changed')} />
+      <h1>
+        Creatable Multiple Select
+      </h1>
       <Creatable
           options={stateOptions}
           onChange={action('Multiple creatable changed')}
-          isMulti
-          />
+          isMulti />
     </div>
   ));
-
