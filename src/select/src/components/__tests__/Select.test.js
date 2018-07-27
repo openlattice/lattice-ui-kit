5abaece3122a32ee0168a3e1a481cad8571eb433
Solomon Tang <solomoncztang@gmail.com>
2018-07-27 15:58:12 -0700

    WIP test

--- src/select/src/components/__tests__/Select.test.js
+++ src/select/src/components/__tests__/Select.test.js
@@ -4,6 +4,14 @@ import toJson from 'enzyme-to-json';
 import 'jest-styled-components';
 import Select from '../Select';
 
+const OPTIONS = [
+  { label: '0', value: 'zero' },
+  { label: '1', value: 'one' },
+  { label: '2', value: 'two' },
+  { label: '3', value: 'three' },
+  { label: '4', value: 'four' },
+];
+
 describe('Select', () => {
 
   it('render matches snapshot', () => {
@@ -14,6 +22,15 @@ describe('Select', () => {
   it('attrs classNamePrefix is set to "lattice-select"', () => {
     const wrapper = mount(<Select />);
     expect(wrapper.instance().attrs.classNamePrefix).toEqual('lattice-select');
+  });
+
+  it('should show controlled value', () => {
+    const wrapper = mount(
+      <Select
+          options={OPTIONS}
+          value={OPTIONS[0]} />
+    );
+    expect(wrapper.find('Control').text()).toBe('0');
   });
 
 });
