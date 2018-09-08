dcfb8445fe993d8ed4ab4fe311a65734f6dcaf3c
Hristo Oskov <hristo.oskov@gmail.com>
2018-09-08 16:16:57 -0700

    exporting the modal

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const version :string = __VERSION__;
 export { Creatable, Select } from './select';
 export { version };
 export { default as Button } from './button';
+export { default as Modal } from './modal';
 export { default as Overlay } from './overlay';
 export { default as Portal } from './portal';
 export default {
