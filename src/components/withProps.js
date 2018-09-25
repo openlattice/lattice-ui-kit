b45f66bc0bbb760fa6db3a95d3cd5ee109efac34
Solomon Tang <solomoncztang@gmail.com>
2018-09-25 12:44:58 -0700

    Linting

--- src/components/withProps.js
+++ src/components/withProps.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const withProps = (WrappedComponent, hocProps) => {
   const LatticeWrapper = props => (
-    <WrappedComponent
-        {...hocProps}
-        {...props} />
+    <WrappedComponent {...hocProps} {...props} />
   );
   return LatticeWrapper;
 };
