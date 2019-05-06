69b9a967eb7516ccd28f42e08e34189e03b11579
Solomon Tang <solomoncztang@gmail.com>
2019-05-06 16:02:27 -0700

    Shallow render DatePicker snapshot

--- src/datetime/src/components/DatePicker.test.js
+++ src/datetime/src/components/DatePicker.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import LatticeDatePicker, { props } from './DatePicker';
 
 describe('DatePicker', () => {
+
+  test('render matches snapshot', () => {
+    // Use shallow render to avoid the current date from imposing on snapshot
+    const tree = shallow(<LatticeDatePicker />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
 
   test('should render internal DatePicker', () => {
     const wrapper = mount(<LatticeDatePicker />);
