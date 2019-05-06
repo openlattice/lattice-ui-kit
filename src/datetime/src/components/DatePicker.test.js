8cb427c9d8523e2136fd7c119a17eb9dee6b49e3
Solomon Tang <solomoncztang@gmail.com>
2019-05-06 15:58:53 -0700

    Remove snapshot for DatePicker

--- src/datetime/src/components/DatePicker.test.js
+++ src/datetime/src/components/DatePicker.test.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import DatePicker, { props } from './DatePicker';
+import LatticeDatePicker, { props } from './DatePicker';
 
 describe('DatePicker', () => {
 
-  test('render matches snapshot', () => {
-    const tree = mount(<DatePicker />);
-    expect(toJson(tree)).toMatchSnapshot();
-  });
-
   test('should render internal DatePicker', () => {
-    const wrapper = mount(<DatePicker />);
+    const wrapper = mount(<LatticeDatePicker />);
     expect(wrapper.find('DatePicker').exists()).toBeTruthy();
   });
 
   test('internal DatePicker should have selectProps', () => {
-    const wrapper = mount(<DatePicker />);
+    const wrapper = mount(<LatticeDatePicker />);
     const datePickerProps = wrapper.find('DatePicker').props();
     expect(datePickerProps.selectProps).toEqual(props.selectProps);
   });
