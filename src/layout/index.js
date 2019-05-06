ab9b1d61fb9f9a288e3886e98ba9cc740c75289b
Solomon Tang <solomoncztang@gmail.com>
2019-05-06 15:35:13 -0700

    Add CardStack

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,9 @@
 import Card from './src/components/Card/Card';
 import CardSegment from './src/components/CardSegment/CardSegment';
+import CardStack from './src/components/CardStack/CardStack';
 
 export {
   Card,
-  CardSegment
+  CardSegment,
+  CardStack,
 };
