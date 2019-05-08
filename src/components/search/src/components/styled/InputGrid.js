b6ff71c7ae16088b0d8ad7ef12aa77819ad4e0e0
Solomon Tang <solomoncztang@gmail.com>
2019-05-08 14:58:35 -0700

    Add InputGrid

---
+++ src/components/search/src/components/styled/InputGrid.js
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components';
+
+const getGridTemplateColumns = ({ columns = 4 }) => css`
+  grid-template-columns: repeat(${columns}, minmax(100px, 1fr));
+`;
+
+const InputGrid = styled.div`
+  display: grid;
+  grid-gap: 20px 30px;
+  align-items: flex-end;
+  ${getGridTemplateColumns}
+`;
+
+InputGrid.displayName = 'InputGrid';
+
+export default InputGrid;
