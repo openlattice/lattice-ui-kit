cb546f891e072ac543414da9d615e17d3143807f
Solomon Tang <solomoncztang@gmail.com>
2019-05-08 15:24:03 -0700

    Add Title

---
+++ src/components/search/src/components/styled/Title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: normal;
+  margin: 0 0 30px 0;
+`;
+
+Title.displayName = 'Title';
+
+export default Title;
