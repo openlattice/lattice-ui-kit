2585ae6402ea762462ac1bf4f9c5d0d7987958e5
Hristo Oskov <hristo@openlattice.com>
2019-05-09 15:05:58 -0700

    splitting into chapters

--- src/modal/stories/index.stories.js
+++ src/modal/stories/index.stories.js
@@ -34,165 +34,151 @@ class ModalExample extends Component {
 }
 
 storiesOf('Modal', module)
-  .add('Basic Modal', () => (
-    <Fragment>
-      <ModalExample>
-        {
-          ({ hide, isVisible, show }) => (
-            <Fragment>
-              <Modal
-                  isVisible={isVisible}
-                  onClose={hide}
-                  textPrimary="Confirm"
-                  textSecondary="Cancel"
-                  textTitle="Metallica, Master of Puppets - March 3, 1986">
-                <p>
-                  March 3, 1986
-                </p>
-                <a href="https://openlattice.com">
-                  https://openlattice.com
-                </a>
-              </Modal>
-              <Button onClick={show}>
-                Show Modal
-              </Button>
-            </Fragment>
-          )
-        }
-      </ModalExample>
-      <br />
-      <br />
-      <ModalExample>
-        {
-          ({ hide, isVisible, show }) => (
-            <Fragment>
-              <Modal
-                  isVisible={isVisible}
-                  onClose={hide}
-                  shouldStretchButtons
-                  textPrimary="Confirm"
-                  textTitle="Metallica, Master of Puppets - March 3, 1986">
-                <p>
-                  March 3, 1986
-                </p>
-                <a href="https://openlattice.com">
-                  https://openlattice.com
-                </a>
-              </Modal>
-              <Button onClick={show}>
-                Show Modal - stretchy primary button
-              </Button>
-            </Fragment>
-          )
-        }
-      </ModalExample>
-      <br />
-      <br />
-      <ModalExample>
-        {
-          ({ hide, isVisible, show }) => (
-            <Fragment>
-              <Modal
-                  isVisible={isVisible}
-                  onClose={hide}
-                  shouldStretchButtons
-                  textSecondary="Cancel"
-                  textTitle="Metallica, Master of Puppets - March 3, 1986">
-                <p>
-                  March 3, 1986
-                </p>
-                <a href="https://openlattice.com">
-                  https://openlattice.com
-                </a>
-              </Modal>
-              <Button onClick={show}>
-                Show Modal - stretchy secondary button
-              </Button>
-            </Fragment>
-          )
-        }
-      </ModalExample>
-      <br />
-      <br />
-      <ModalExample>
-        {
-          ({ hide, isVisible, show }) => (
-            <Fragment>
-              <Modal
-                  isVisible={isVisible}
-                  onClose={hide}
-                  shouldStretchButtons
-                  textPrimary="Confirm"
-                  textSecondary="Cancel"
-                  textTitle="Metallica, Master of Puppets - March 3, 1986">
-                <p>
-                  March 3, 1986
-                </p>
-                <a href="https://openlattice.com">
-                  https://openlattice.com
-                </a>
-              </Modal>
-              <Button onClick={show}>
-                Show Modal - 2 stretchy buttons
-              </Button>
-            </Fragment>
-          )
-        }
-      </ModalExample>
-      <br />
-      <br />
-      <ModalExample>
-        {
-          ({ hide, isVisible, show }) => (
-            <Fragment>
-              <Modal
-                  isVisible={isVisible}
-                  onClose={hide}
-                  shouldBeCentered={false}
-                  textPrimary="Confirm"
-                  textSecondary="Cancel"
-                  textTitle="Metallica, Master of Puppets - March 3, 1986">
-                <p>
-                  March 3, 1986
-                </p>
-                <a href="https://openlattice.com">
-                  https://openlattice.com
-                </a>
-              </Modal>
-              <Button onClick={show}>
-                Show Modal - align top
-              </Button>
-            </Fragment>
-          )
-        }
-      </ModalExample>
-      <br />
-      <br />
-      <ModalExample>
-        {
-          ({ hide, isVisible, show }) => (
-            <Fragment>
-              <Modal
-                  isVisible={isVisible}
-                  onClose={hide}
-                  shouldBeCentered={false}
-                  shouldCloseOnOutsideClick={false}
-                  textPrimary="Confirm"
-                  textSecondary="Cancel"
-                  textTitle="Metallica, Master of Puppets - March 3, 1986">
-                <p>
-                  March 3, 1986
-                </p>
-                <a href="https://openlattice.com">
-                  https://openlattice.com
-                </a>
-              </Modal>
-              <Button onClick={show}>
-                Show Modal - don&apos;t hide on outside click
-              </Button>
-            </Fragment>
-          )
-        }
-      </ModalExample>
-    </Fragment>
+  .add('basic', () => (
+    <ModalExample>
+      {
+        ({ hide, isVisible, show }) => (
+          <Fragment>
+            <Modal
+                isVisible={isVisible}
+                onClose={hide}
+                textPrimary="Confirm"
+                textSecondary="Cancel"
+                textTitle="Metallica, Master of Puppets - March 3, 1986">
+              <p>
+                March 3, 1986
+              </p>
+              <a href="https://openlattice.com">
+                https://openlattice.com
+              </a>
+            </Modal>
+            <Button onClick={show}>Show Modal</Button>
+          </Fragment>
+        )
+      }
+    </ModalExample>
+  ))
+  .add('stretchy buttons', () => (
+    <ModalExample>
+      {
+        ({ hide, isVisible, show }) => (
+          <Fragment>
+            <Modal
+                isVisible={isVisible}
+                onClose={hide}
+                shouldStretchButtons
+                textPrimary="Confirm"
+                textSecondary="Cancel"
+                textTitle="Metallica, Master of Puppets - March 3, 1986">
+              <p>
+                March 3, 1986
+              </p>
+              <a href="https://openlattice.com">
+                https://openlattice.com
+              </a>
+            </Modal>
+            <Button onClick={show}>Show Modal</Button>
+          </Fragment>
+        )
+      }
+    </ModalExample>
+  ))
+  .add('stretchy primary button', () => (
+    <ModalExample>
+      {
+        ({ hide, isVisible, show }) => (
+          <Fragment>
+            <Modal
+                isVisible={isVisible}
+                onClose={hide}
+                shouldStretchButtons
+                textPrimary="Confirm"
+                textTitle="Metallica, Master of Puppets - March 3, 1986">
+              <p>
+                March 3, 1986
+              </p>
+              <a href="https://openlattice.com">
+                https://openlattice.com
+              </a>
+            </Modal>
+            <Button onClick={show}>Show Modal</Button>
+          </Fragment>
+        )
+      }
+    </ModalExample>
+  ))
+  .add('stretchy secondary button', () => (
+    <ModalExample>
+      {
+        ({ hide, isVisible, show }) => (
+          <Fragment>
+            <Modal
+                isVisible={isVisible}
+                onClose={hide}
+                shouldStretchButtons
+                textSecondary="Cancel"
+                textTitle="Metallica, Master of Puppets - March 3, 1986">
+              <p>
+                March 3, 1986
+              </p>
+              <a href="https://openlattice.com">
+                https://openlattice.com
+              </a>
+            </Modal>
+            <Button onClick={show}>Show Modal</Button>
+          </Fragment>
+        )
+      }
+    </ModalExample>
+  ))
+  .add('align top', () => (
+    <ModalExample>
+      {
+        ({ hide, isVisible, show }) => (
+          <Fragment>
+            <Modal
+                isVisible={isVisible}
+                onClose={hide}
+                shouldBeCentered={false}
+                textPrimary="Confirm"
+                textSecondary="Cancel"
+                textTitle="Metallica, Master of Puppets - March 3, 1986">
+              <p>
+                March 3, 1986
+              </p>
+              <a href="https://openlattice.com">
+                https://openlattice.com
+              </a>
+            </Modal>
+            <Button onClick={show}>Show Modal</Button>
+          </Fragment>
+        )
+      }
+    </ModalExample>
+  ))
+  .add('outside click does not close modal', () => (
+    <ModalExample>
+      {
+        ({ hide, isVisible, show }) => (
+          <Fragment>
+            <Modal
+                isVisible={isVisible}
+                onClose={hide}
+                shouldBeCentered={false}
+                shouldCloseOnOutsideClick={false}
+                textPrimary="Confirm"
+                textSecondary="Cancel"
+                textTitle="Metallica, Master of Puppets - March 3, 1986">
+              <p>
+                March 3, 1986
+              </p>
+              <a href="https://openlattice.com">
+                https://openlattice.com
+              </a>
+            </Modal>
+            <Button onClick={show}>Show Modal</Button>
+          </Fragment>
+        )
+      }
+    </ModalExample>
   ));
