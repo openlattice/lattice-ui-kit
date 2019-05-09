7a7e3e02223b346ab9f22205a61511562e247a23
Solomon Tang <solomoncztang@gmail.com>
2019-05-09 16:19:15 -0700

    Add FilterFieldDefinition type

--- src/components/search/types.js
+++ src/components/search/types.js
@@ -1,8 +1,16 @@
 // @flow
+import type { Map } from 'immutable';
 
 export type SearchFieldDefinition = {|
   id :string;
   label :string;
   type ?:string;
   value ?:string;
+|};
+
+export type FilterFieldDefinition = {|
+  filter :(searchResult :Map, filter :FilterFieldDefinition) => boolean;
+  id :string;
+  label :string;
+  value :string[];
 |};
