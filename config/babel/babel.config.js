48e2fae580da297c60fd898b4432d2e21e28e582
Solomon Tang <solomoncztang@gmail.com>
2019-05-22 13:02:37 -0700

    Do not prefix fileName in displayNames

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -3,7 +3,9 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-runtime',
-    'babel-plugin-styled-components',
+    ['babel-plugin-styled-components', {
+      fileName: false
+    }]
   ],
   presets: [
     '@babel/preset-env',
