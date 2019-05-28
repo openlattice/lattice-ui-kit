ea2c003a363938505644ea908185ac34d2002634
Solomon Tang <solomoncztang@gmail.com>
2019-05-28 12:24:19 -0700

    Update banner story

--- src/banner/stories/index.stories.js
+++ src/banner/stories/index.stories.js
@@ -3,12 +3,15 @@ import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 
 import Banner from '..';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNarwhal } from '@fortawesome/pro-regular-svg-icons';
 
 const StyledDiv = styled.div`
   display: flex;
-  flex-wrap;
+  flex: 1;
   font-size: 24px;
   min-width: 0;
+  justify-content: space-evenly;
 `;
 
 const Name = styled.strong`
@@ -53,6 +56,18 @@ class BannerController extends Component {
 }
 
 storiesOf('Banner', module)
-  .add('Default', () => (
-    <BannerController />
+  .add('default', () => (
+    <>
+      <Banner isOpen mode="default">This is a banner</Banner>
+      <Banner isOpen mode="danger">This is an error</Banner>
+      <Banner isOpen mode="warning">This is a warning</Banner>
+      <Banner isOpen mode="success">This is a success</Banner>
+      <Banner isOpen icon={<FontAwesomeIcon icon={faNarwhal} />}>This is a custom icon</Banner>
+    </>
+  ))
+  .add('Custom controlled', () => (
+    <>
+      <BannerController />
+      <div>I get pushed</div>
+    </>
   ));
