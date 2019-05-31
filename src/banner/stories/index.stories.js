a484c961eb31394689b31da6e42191231fa84929
Solomon Tang <solomoncztang@gmail.com>
2019-05-30 18:24:52 -0700

    Add sticky banner story

--- src/banner/stories/index.stories.js
+++ src/banner/stories/index.stories.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
-
-import Banner from '..';
+import { LoremIpsum } from 'lorem-ipsum';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNarwhal } from '@fortawesome/pro-regular-svg-icons';
+
+import Banner from '..';
+import Button from '../../button';
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
 
 const StyledDiv = styled.div`
   display: flex;
@@ -27,6 +40,15 @@ const Birthdate = styled.span`
   white-space: nowrap;
 `;
 
+const Container = styled.div`
+  max-height: 200px;
+  overflow-y: scroll;
+`;
+
+const Filler = styled.div`
+  height: 400;
+`;
+
 class BannerController extends Component {
   state = {
     open: false
@@ -43,7 +65,7 @@ class BannerController extends Component {
     const { open } = this.state;
     return (
       <>
-        <button type="button" onClick={this.handleOpenToggle}>Toggle</button>
+        <Button type="button" mode="primary" onClick={this.handleOpenToggle}>Toggle</Button>
         <Banner isOpen={open}>
           <StyledDiv>
             <Name>WERBENJAGERMANJENSEN, SMITTY</Name>
@@ -63,11 +85,18 @@ storiesOf('Banner', module)
       <Banner isOpen mode="warning">This is a warning</Banner>
       <Banner isOpen mode="success">This is a success</Banner>
       <Banner isOpen icon={<FontAwesomeIcon icon={faNarwhal} fixedWidth />}>This is a custom icon</Banner>
+      <br />
+      <Container>
+        <Banner isOpen sticky>This is a sticky banner. Scroll down to observe</Banner>
+        <Filler>
+          { lorem.generateParagraphs(4) }
+        </Filler>
+      </Container>
     </>
   ))
   .add('Custom controlled', () => (
     <>
       <BannerController />
-      <div>I get pushed</div>
+      { lorem.generateParagraphs(4) }
     </>
   ));
