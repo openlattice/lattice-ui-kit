d9be5aa5f60e35b7e8193c616344cab1f5b6c78b
Solomon Tang <solomoncztang@gmail.com>
2019-06-20 11:06:08 -0700

    Remove unused imports

--- src/layout/src/components/CardHeader/CardHeader.test.js
+++ src/layout/src/components/CardHeader/CardHeader.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import toJson from 'enzyme-to-json';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import CardHeader from './CardHeader';
 import {
