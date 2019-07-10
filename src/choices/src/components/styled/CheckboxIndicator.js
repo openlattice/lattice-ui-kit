d4f167c99de4c8f75296ee965eeca843ca973707
Solomon Tang <solomoncztang@gmail.com>
2019-07-10 13:47:01 -0700

    Update Indicator styles

--- src/choices/src/components/styled/CheckboxIndicator.js
+++ src/choices/src/components/styled/CheckboxIndicator.js
@@ -1,40 +1,67 @@
 // @flow
 
 import styled, { css } from 'styled-components';
-import { PURPLES, WHITE } from '../../../../colors';
-import type { IndicatorProps } from '../../../types';
+import { PURPLES, WHITE, NEUTRALS } from '../../../../colors';
 
-const getAfterStyles = ({ checked } :IndicatorProps) => {
-  if (!checked) {
-    return css`
-      display: none;
-    `;
-  }
-
-  return css`
-    content: '';
-    border: solid ${WHITE};
-    border-width: 0 2px 2px 0;
-    height: 8px;
-    left: 8px;
-    position: absolute;
-    top: 4px;
-    transform: rotate(45deg);
-    width: 3px;
-  `;
-};
+const afterStyles = css`
+  content: '';
+  border: solid ${WHITE};
+  border-width: 0 2px 2px 0;
+  height: 8px;
+  left: 8px;
+  position: absolute;
+  top: 4px;
+  width: 3px;
+  transform: rotate(45deg) scale(1);
+  transition: transform 0.2s ease-in-out, opacity 0.2s ease-in-out;
+`;
 
 const CheckboxIndicator = styled.div`
+  background-color: ${PURPLES[6]};
+  border-radius: 3px;
+  height: 20px;
+  left: 0;
   position: absolute;
   top: 0;
-  left: 0;
-  height: 20px;
+  transition: background-color 0.2s ease-in-out;
   width: 20px;
-  background: ${PURPLES[6]};
-  border-radius: 3px;
 
-  :after {
-    ${getAfterStyles}
+  input:hover ~ &,
+  input:focus ~ & {
+    background-color: ${NEUTRALS[1]};
+  }
+
+  input:checked ~ & {
+    background-color: ${PURPLES[2]};
+  }
+
+  input:checked:hover ~ &,
+  input:checked:focus ~ & {
+    background-color: ${PURPLES[1]};
+  }
+
+  input:disabled ~ & {
+    background-color: ${PURPLES[6]};
+    cursor: not-allowed;
+  }
+
+  input:checked:disabled ~ & {
+    background-color: ${NEUTRALS[2]};
+  }
+
+  input:not(:checked) ~ &:after,
+  input:checked ~ &:after {
+    ${afterStyles}
+  }
+
+  input:not(:checked) ~ &:after {
+    opacity: 0;
+    transform: rotate(45deg) scale(0);
+  }
+
+  input:checked ~ &:after {
+    opacity: 1;
+    transform: rotate(45deg) scale(1);
   }
 `;
 
--- src/choices/src/components/styled/RadioIndicator.js
+++ src/choices/src/components/styled/RadioIndicator.js
@@ -1,40 +1,66 @@
 // @flow
 
 import styled, { css } from 'styled-components';
-import { PURPLES, WHITE } from '../../../../colors';
-import type { IndicatorProps } from '../../../types';
+import { PURPLES, WHITE, NEUTRALS } from '../../../../colors';
 
-const getAfterStyles = ({ checked } :IndicatorProps) => {
-  if (!checked) {
-    return css`
-      display: none;
-    `;
-  }
-
-  return css`
-    content: '';
-    background-color: ${WHITE};
-    border-radius: 50%;
-    height: 8px;
-    left: 6px;
-    position: absolute;
-    top: 6px;
-    width: 8px;
-  `;
-};
-
+const afterStyles = css`
+  content: '';
+  background-color: ${WHITE};
+  border-radius: 50%;
+  height: 8px;
+  left: 6px;
+  position: absolute;
+  top: 6px;
+  width: 8px;
+  transition: transform 0.2s ease-in-out, opacity 0.2s ease-in-out;
+`;
 
 const RadioIndicator = styled.div`
+  background-color: ${PURPLES[6]};
+  border-radius: 100%;
+  height: 20px;
+  left: 0;
   position: absolute;
   top: 0;
-  left: 0;
-  height: 20px;
+  transition: background-color 0.2s ease-in-out;
   width: 20px;
-  background: ${PURPLES[6]};
-  border-radius: 50%;
 
-  :after {
-    ${getAfterStyles}
+  input:hover ~ &,
+  input:focus ~ & {
+    background-color: ${NEUTRALS[1]};
+  }
+
+  input:checked ~ & {
+    background-color: ${PURPLES[2]};
+  }
+
+  input:checked:hover ~ &,
+  input:checked:focus ~ & {
+    background-color: ${PURPLES[1]};
+  }
+
+  input:disabled ~ & {
+    background-color: ${PURPLES[6]};
+    cursor: not-allowed;
+  }
+
+  input:checked:disabled ~ & {
+    background-color: ${NEUTRALS[2]};
+  }
+
+  input:not(:checked) ~ &:after,
+  input:checked ~ &:after {
+    ${afterStyles}
+  }
+
+  input:not(:checked) ~ &:after {
+    opacity: 0;
+    transform: scale(0);
+  }
+
+  input:checked ~ &:after {
+    opacity: 1;
+    transform: scale(1);
   }
 `;
 
