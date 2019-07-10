bc720034f648ebed32225153aa6cf707eb3a5a29
Solomon Tang <solomoncztang@gmail.com>
2019-07-10 00:15:11 -0700

    Make stateless

--- src/radio/src/components/Radio.js
+++ src/radio/src/components/Radio.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import {
   RadioIndicator,
   RadioInput,
@@ -16,66 +16,36 @@ type Props = {
   readOnly ? :boolean;
 };
 
-type State = {
-  isChecked :boolean;
-}
+const Radio = ({
+  checked,
+  disabled,
+  label,
+  name,
+  readOnly,
+  onChange,
+  value
+} :Props) => (
+  <RadioLabel>
+    <div>{label}</div>
+    <RadioInput
+        checked={checked}
+        disabled={disabled}
+        readOnly={readOnly}
+        name={name}
+        onChange={onChange}
+        value={value} />
+    <RadioIndicator />
+  </RadioLabel>
+);
 
-class Radio extends Component<Props, State> {
-
-  static defaultProps = {
-    checked: undefined,
-    disabled: false,
-    label: undefined,
-    name: undefined,
-    onChange: undefined,
-    readOnly: undefined,
-    value: undefined,
-  };
-
-  state = {
-    isChecked: false
-  };
-
-  getIsChecked = () => {
-    const { checked } = this.props;
-    const { isChecked } = this.state;
-    return checked !== undefined ? checked : isChecked;
-  }
-
-  handleChange = (e :SyntheticInputEvent<HTMLInputElement>) => {
-    const { disabled, onChange, readOnly } = this.props;
-    e.persist();
-    if (!(disabled || readOnly)) {
-      this.setState({ isChecked: e.target.checked });
-      if (onChange) {
-        onChange(e);
-      }
-    }
-  };
-
-  render() {
-    const {
-      disabled,
-      label,
-      name,
-      readOnly,
-      value
-    } = this.props;
-    const isChecked = this.getIsChecked();
-    return (
-      <RadioLabel checked={isChecked}>
-        <div>{label}</div>
-        <RadioInput
-            checked={isChecked}
-            disabled={disabled}
-            readOnly={readOnly}
-            name={name}
-            onChange={this.handleChange}
-            value={value} />
-        <RadioIndicator checked={isChecked} />
-      </RadioLabel>
-    );
-  }
-}
+Radio.defaultProps = {
+  checked: undefined,
+  disabled: false,
+  label: undefined,
+  name: undefined,
+  onChange: undefined,
+  readOnly: undefined,
+  value: undefined,
+};
 
 export default Radio;
