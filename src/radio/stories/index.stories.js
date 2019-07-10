515812dd7797ccb1327d759c801bf09beb3fc854
Solomon Tang <solomoncztang@gmail.com>
2019-07-10 00:15:26 -0700

    Update radio story

--- src/radio/stories/index.stories.js
+++ src/radio/stories/index.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
+import Label from '../../label';
 import Radio from '..';
 import { Card, CardSegment } from '../../layout';
 
@@ -18,23 +19,24 @@ storiesOf('Radio', module)
             flex: '0 1 100%',
             flexDirection: 'column'
           }}>
-            <Radio id="story-default" label="Default" />
-
+            <Label>Showcase</Label>
+            <Radio id="story-unchecked" label="Unchecked" checked={false} />
             <Radio id="story-checked" label="Checked" checked />
-
             <Radio id="story-checkedreadonly" label="Checked readOnly" checked readOnly />
-
             <Radio id="story-disabled" label="Disabled" disabled />
-
             <Radio id="story-disabledchecked" label="Disabled (checked)" disabled checked />
 
-            <Radio
-                id="story-eventhandlers"
-                label="Event handlers (see Actions tab)"
-                onChange={action('onChange')}
-                onBlur={action('onBlur')}
-                onFocus={action('onFocus')} />
-
+          </form>
+          <form style={{
+            display: 'flex',
+            flex: '0 1 100%',
+            flexDirection: 'column'
+          }}>
+            <Label>See Actions tab</Label>
+            <Radio name="group" onChange={action('onChange')} id="story-default" label="Option 1" value="1" />
+            <Radio name="group" onChange={action('onChange')} id="story-checked" label="Option 2" value="2" />
+            <Radio name="group" onChange={action('onChange')} id="story-checkedreadonly" label="Option 3" value="3" />
+            <Radio name="group" onChange={action('onChange')} id="story-disabled" label="Option 4" value="4" />
           </form>
         </CardSegment>
       </Card>
