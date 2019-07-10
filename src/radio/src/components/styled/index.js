7bf14c6bf3f834000c5dfd41ec656202fa5df4b1
Solomon Tang <solomoncztang@gmail.com>
2019-07-10 00:14:53 -0700

    Add transitions

--- src/radio/src/components/styled/index.js
+++ src/radio/src/components/styled/index.js
@@ -13,72 +13,72 @@ const RadioLabel = styled.label`
   position: relative;
 `;
 
-type IndicatorProps = {
-  checked :boolean;
-};
-
-const getAfterStyles = ({ checked } :IndicatorProps) => {
-  if (!checked) {
-    return css`
-      display: none;
-    `;
-  }
-
-  return css`
-    content: '';
-    background-color: ${WHITE};
-    border-radius: 50%;
-    height: 8px;
-    left: 6px;
-    position: absolute;
-    top: 6px;
-    width: 8px;
-  `;
-};
-
-const RadioIndicator = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 20px;
-  width: 20px;
-  background: ${PURPLES[6]};
-  border-radius: 50%;
-
-  :after {
-    ${getAfterStyles}
-  }
-`;
-
 const RadioInput = styled.input.attrs(() => ({
   type: 'radio'
 }))`
   position: absolute;
   z-index: -1;
   opacity: 0;
-  vertical-align: middle;
+`;
 
-  :hover + div,
-  :focus + div {
-    background: ${NEUTRALS[1]};
+const afterStyles = css`
+  content: '';
+  background-color: ${WHITE};
+  border-radius: 50%;
+  height: 8px;
+  left: 6px;
+  position: absolute;
+  top: 6px;
+  width: 8px;
+  transition: transform 0.2s ease-in-out, opacity 0.2s ease-in-out;
+`;
+
+const RadioIndicator = styled.div`
+  background-color: ${PURPLES[6]};
+  border-radius: 100%;
+  height: 20px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  transition: background-color 0.1s ease-in-out;
+  width: 20px;
+
+  ${RadioInput}:hover ~ &,
+  ${RadioInput}:focus ~ & {
+    background-color: ${NEUTRALS[1]};
   }
 
-  :checked + div {
-    background: ${PURPLES[2]};
+  ${RadioInput}:checked ~ & {
+    background-color: ${PURPLES[2]};
   }
 
-  :checked:hover + div,
-  :checked:focus + div {
-    background: ${PURPLES[1]};
+  ${RadioInput}:checked:hover ~ &,
+  ${RadioInput}:checked:focus ~ & {
+    background-color: ${PURPLES[1]};
   }
 
-  :disabled + div {
-    background: ${PURPLES[6]};
+  ${RadioInput}:disabled ~ & {
+    background-color: ${PURPLES[6]};
     cursor: not-allowed;
   }
 
-  :checked:disabled + div {
-    background: ${NEUTRALS[2]};
+  ${RadioInput}:checked:disabled ~ & {
+    background-color: ${NEUTRALS[2]};
+  }
+
+  ${RadioInput}:not(:checked) ~ &:after,
+  ${RadioInput}:checked ~ &:after {
+    ${afterStyles}
+  }
+
+  ${RadioInput}:not(:checked) ~ &:after {
+    opacity: 0;
+    transform: scale(0);
+  }
+
+  ${RadioInput}:checked ~ &:after {
+    opacity: 1;
+    transform: scale(1);
   }
 `;
 
