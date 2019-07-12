5e109a6ec79ae2f3165e0b7cb1bcc1be111facb8
Solomon Tang <solomoncztang@gmail.com>
2019-07-12 16:58:18 -0700

    Condense story with .addDecorator

--- src/stepper/stories/index.stories.js
+++ src/stepper/stories/index.stories.js
@@ -7,7 +7,7 @@ import useStepState from './components/useStepState';
 
 import { Stepper, Step } from '..';
 import Button from '../../button';
-import { Card, CardStack, CardSegment } from '../../layout';
+import { Card, CardSegment } from '../../layout';
 
 const ButtonGroup = styled.div`
   button {
@@ -18,15 +18,12 @@ const ButtonGroup = styled.div`
 const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
 
 storiesOf('Stepper', module)
-  .add('Horizontal stepper', () => {
-
+  .addDecorator((Story) => {
     const [currentStep, setStep, nextStep, prevStep] = useStepState(steps.length);
     return (
       <Card>
         <CardSegment vertical>
-          <Stepper activeStep={currentStep}>
-            { steps.map(title => <Step>{title}</Step>) }
-          </Stepper>
+          <Story currentStep={currentStep} setStep={setStep} />
           <ButtonGroup>
             <Button mode="secondary" disabled={currentStep === 0} onClick={prevStep}>Back</Button>
             <Button mode="primary" disabled={currentStep === steps.length - 1} onClick={nextStep}>Next</Button>
@@ -36,66 +33,25 @@ storiesOf('Stepper', module)
       </Card>
     );
   })
-  .add('Vertical stepper', () => {
-
-    const [currentStep, setStep, nextStep, prevStep] = useStepState(steps.length);
-    return (
-      <Card>
-        <CardSegment vertical>
-          <Stepper activeStep={currentStep} vertical>
-            { steps.map(title => <Step>{title}</Step>) }
-          </Stepper>
-          <ButtonGroup>
-            <Button mode="secondary" disabled={currentStep === 0} onClick={prevStep}>Back</Button>
-            <Button mode="primary" disabled={currentStep === steps.length - 1} onClick={nextStep}>Next</Button>
-            <Button onClick={() => setStep(0)}>Reset</Button>
-          </ButtonGroup>
-        </CardSegment>
-      </Card>
-    );
-  })
-  .add('onClick', () => {
-
-    const [currentStep, setStep, nextStep, prevStep] = useStepState(steps.length);
-    return (
-      <CardStack>
-        <Card>
-          <CardSegment vertical>
-            <Stepper activeStep={currentStep}>
-              { steps.map((title, index) => <Step onClick={() => setStep(index)}>{title}</Step>) }
-            </Stepper>
-          </CardSegment>
-        </Card>
-        <Card>
-          <CardSegment vertical>
-            <Stepper activeStep={currentStep} vertical>
-              { steps.map((title, index) => <Step onClick={() => setStep(index)}>{title}</Step>) }
-            </Stepper>
-            <ButtonGroup>
-              <Button mode="secondary" disabled={currentStep === 0} onClick={prevStep}>Back</Button>
-              <Button mode="primary" disabled={currentStep === steps.length - 1} onClick={nextStep}>Next</Button>
-              <Button onClick={() => setStep(0)}>Reset</Button>
-            </ButtonGroup>
-          </CardSegment>
-        </Card>
-      </CardStack>
-    );
-  })
-  .add('Sequential stepper', () => {
-
-    const [currentStep, setStep, nextStep, prevStep] = useStepState(steps.length);
-    return (
-      <Card>
-        <CardSegment vertical>
-          <Stepper activeStep={currentStep} sequential>
-            { steps.map((title, index) => <Step onClick={() => setStep(index)}>{title}</Step>) }
-          </Stepper>
-          <ButtonGroup>
-            <Button mode="secondary" disabled={currentStep === 0} onClick={prevStep}>Back</Button>
-            <Button mode="primary" disabled={currentStep === steps.length - 1} onClick={nextStep}>Next</Button>
-            <Button onClick={() => setStep(0)}>Reset</Button>
-          </ButtonGroup>
-        </CardSegment>
-      </Card>
-    );
-  });
+  .add('Horizontal stepper', ({ currentStep }) => (
+    <Stepper activeStep={currentStep}>
+      { steps.map(title => <Step key={title}>{title}</Step>) }
+    </Stepper>
+  ))
+  .add('Vertical stepper', ({ currentStep }) => (
+    <Stepper activeStep={currentStep} vertical>
+      { steps.map(title => <Step key={title}>{title}</Step>) }
+    </Stepper>
+  ))
+  .add('Sequential stepper', ({ currentStep, setStep }) => (
+    <Stepper activeStep={currentStep} sequential>
+      { steps.map((title, index) => <Step key={title} onClick={() => setStep(index)}>{title}</Step>) }
+    </Stepper>
+  ))
+  .add('Custom step', ({ currentStep, setStep }) => (
+    <Stepper activeStep={currentStep} sequential>
+      { steps.map((title, index) => (
+        <Step key={title} onClick={() => setStep(index)}>{title}</Step>
+      ))}
+    </Stepper>
+  ));
