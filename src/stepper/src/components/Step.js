e41ecbfee3bea8427cfc4513274f009a3f084108
Solomon Tang <solomoncztang@gmail.com>
2019-07-12 16:58:05 -0700

    Step composes props to children

--- src/stepper/src/components/Step.js
+++ src/stepper/src/components/Step.js
@@ -1,37 +1,82 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import type { ChildrenArray } from 'react';
 
 import StepIcon from './StepIcon';
-import { StepWrapper } from './styled';
+import { StepWrapper, StepLabel } from './styled';
 
 type Props = {
   index :number;
   active :boolean;
   complete :boolean;
+  disabled ? :boolean;
   children :ChildrenArray<any>;
-  onClick ? :(e :SyntheticEvent<HTMLDivElement>) => void;
 };
 
-const Step = ({
-  active,
-  complete,
-  children,
-  index,
-  onClick
-} :Props) => (
-  <StepWrapper onClick={onClick}>
-    <StepIcon
-        index={index}
-        active={active}
-        complete={complete} />
-    {children}
-  </StepWrapper>
-);
+class Step extends Component<Props> {
 
-Step.defaultProps = {
-  onClick: undefined
-};
+  static defaultProps = {
+    disabled: false
+  };
+
+  renderDefault = () => {
+    const {
+      active,
+      children,
+      complete,
+      index,
+    } = this.props;
+    return (
+      <>
+        <StepIcon
+            active={active}
+            complete={complete}
+            index={index} />
+        <StepLabel active={active}>{children}</StepLabel>
+      </>
+    );
+  }
+
+  renderComposed = () => {
+    const {
+      active,
+      children,
+      complete,
+      disabled,
+      index,
+    } = this.props;
+
+    return React.Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        return React.cloneElement(child, {
+          active,
+          complete,
+          disabled,
+          index,
+          ...child.props
+        });
+      }
+
+      return child;
+    });
+
+  }
+
+  render() {
+    const {
+      children
+    } = this.props;
+    return (
+      <StepWrapper>
+        {
+          React.isValidElement(children)
+            ? this.renderComposed()
+            : this.renderDefault()
+        }
+      </StepWrapper>
+    );
+  }
+}
 
 export default Step;
