820487fa34e780583366c134e40b62a076c1be85
Solomon Tang <solomoncztang@gmail.com>
2019-07-16 16:53:50 -0700

    Add more style tests

--- src/stepper/src/components/Step.test.js
+++ src/stepper/src/components/Step.test.js
@@ -12,16 +12,25 @@ describe('Step', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  describe('Default', () => {
+  describe('styles', () => {
+    test('default styles', () => {
+      const wrapper = mount(<Step index={0}>Step 1</Step>);
+      expect(wrapper.find(StepWrapper)).toHaveStyleRule('display', 'flex');
+      expect(wrapper.find(StepWrapper)).toHaveStyleRule('align-items', 'center');
+      expect(wrapper.find(StepWrapper)).toHaveStyleRule('padding', '10px');
+    });
+
     test('onClick adds pointer styles', () => {
       const wrapper = mount(<Step index={0} onClick={() => {}}>Step 1</Step>);
       expect(wrapper.find(StepWrapper)).toHaveStyleRule('cursor', 'pointer');
     });
-
-    test('render StepIcon and StepLabel', () => {
-      const wrapper = shallow(<Step index={0}>Step 1</Step>);
-      expect(wrapper.find(StepIcon).prop('index')).toBe(0);
-      expect(wrapper.find(StepLabel).text()).toBe('Step 1');
-    });
   });
+
+
+  test('render StepIcon and StepLabel', () => {
+    const wrapper = shallow(<Step index={0}>Step 1</Step>);
+    expect(wrapper.find(StepIcon).prop('index')).toBe(0);
+    expect(wrapper.find(StepLabel).text()).toBe('Step 1');
+  });
+
 });
