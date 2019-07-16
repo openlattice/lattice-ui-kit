95bdc3a5c906445c6068558a3cc2db17609018c1
Solomon Tang <solomoncztang@gmail.com>
2019-07-16 16:42:57 -0700

    Do not compose Step children

--- src/stepper/src/components/Step.js
+++ src/stepper/src/components/Step.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
 import type { ChildrenArray } from 'react';
 
 import StepIcon from './StepIcon';
@@ -10,75 +10,27 @@ type Props = {
   active :boolean;
   children :ChildrenArray<any>;
   complete :boolean;
-  disabled ? :boolean;
   index :number;
   onClick :(e :SyntheticEvent<HTMLElement>) => void;
 };
 
-class Step extends Component<Props> {
-
-  static defaultProps = {
-    disabled: false
-  };
-
-  renderDefault = () => {
-    const {
-      active,
-      children,
-      complete,
-      index,
-    } = this.props;
-    return (
-      <>
-        <StepIcon
-            active={active}
-            complete={complete}
-            index={index} />
-        <StepLabel active={active}>{children}</StepLabel>
-      </>
-    );
-  }
-
-  renderComposed = () => {
-    const {
-      active,
-      children,
-      complete,
-      disabled,
-      index,
-    } = this.props;
-
-    return React.Children.map(children, (child) => {
-      if (React.isValidElement(child)) {
-        return React.cloneElement(child, {
-          active,
-          complete,
-          disabled,
-          index,
-          ...child.props
-        });
-      }
-
-      return child;
-    });
-
-  }
-
-  render() {
-    const {
-      children,
-      onClick
-    } = this.props;
-    return (
-      <StepWrapper onClick={onClick}>
-        {
-          React.isValidElement(children)
-            ? this.renderComposed()
-            : this.renderDefault()
-        }
-      </StepWrapper>
-    );
-  }
-}
+const Step = (props :Props) => {
+  const {
+    active,
+    children,
+    complete,
+    index,
+    onClick
+  } = props;
+  return (
+    <StepWrapper onClick={onClick}>
+      <StepIcon
+          active={active}
+          complete={complete}
+          index={index} />
+      <StepLabel active={active}>{children}</StepLabel>
+    </StepWrapper>
+  );
+};
 
 export default Step;
