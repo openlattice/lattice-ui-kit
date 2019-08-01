532b72eafcfdcf16750ce6287ead35a4dc911131
Solomon Tang <solomoncztang@gmail.com>
2019-07-31 19:29:08 -0700

    update snapshot

--- src/datetime/src/components/TimePicker.test.js
+++ src/datetime/src/components/TimePicker.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import TimePicker, { defaultProps } from './TimePicker';
+import LatticeTimePicker, { defaultProps } from './TimePicker';
 
 describe('TimePicker', () => {
 
   test('render matches snapshot', () => {
-    const tree = shallow(<TimePicker />);
+    const tree = shallow(<LatticeTimePicker />);
     expect(toJson(tree)).toMatchSnapshot();
   });
 
   test('should render internal TimePicker', () => {
-    const wrapper = mount(<TimePicker />);
+    const wrapper = mount(<LatticeTimePicker />);
     expect(wrapper.find('TimePicker').exists()).toBeTruthy();
   });
 
   test('internal TimePicker should have selectProps', () => {
-    const wrapper = mount(<TimePicker />);
+    const wrapper = mount(<LatticeTimePicker />);
     const timePickerProps = wrapper.find('TimePicker').props();
     expect(timePickerProps.selectProps).toEqual(defaultProps.selectProps);
   });
