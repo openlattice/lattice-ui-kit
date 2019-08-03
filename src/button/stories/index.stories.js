f6adb2680fbe7fcc472287f53d30a9bba93b98a9
Hristo Oskov <hristo@openlattice.com>
2019-08-02 18:04:52 -0700

    updating button story with new buttons

--- src/button/stories/index.stories.js
+++ src/button/stories/index.stories.js
@@ -30,6 +30,8 @@ storiesOf('Button', module)
           <Button onClick={action('button clicked')}>Default</Button>
           <Button mode="primary" onClick={action('button clicked')}>Primary</Button>
           <Button mode="secondary" onClick={action('button clicked')}>Secondary</Button>
+          <Button mode="positive" onClick={action('button clicked')}>Positive</Button>
+          <Button mode="negative" onClick={action('button clicked')}>Negative</Button>
           <Button mode="subtle" onClick={action('button clicked')}>Subtle</Button>
         </ButtonRow>
       </CardSegment>
@@ -37,8 +39,6 @@ storiesOf('Button', module)
         <h1>Disabled</h1>
         <ButtonRow>
           <Button disabled onClick={action('button clicked')}>Default</Button>
-          <Button mode="primary" disabled onClick={action('button clicked')}>Primary</Button>
-          <Button mode="secondary" disabled onClick={action('button clicked')}>Secondary</Button>
           <Button mode="subtle" disabled onClick={action('button clicked')}>Subtle</Button>
         </ButtonRow>
       </CardSegment>
@@ -46,21 +46,18 @@ storiesOf('Button', module)
         <h1>isLoading</h1>
         <ButtonRow>
           <Button isLoading onClick={action('button clicked')}>Default</Button>
-          <Button mode="primary" isLoading onClick={action('button clicked')}>Primary</Button>
-          <Button mode="secondary" isLoading onClick={action('button clicked')}>Secondary</Button>
           <Button mode="subtle" isLoading onClick={action('button clicked')}>Subtle</Button>
         </ButtonRow>
       </CardSegment>
       <CardSegment vertical>
         <h1>Truncate</h1>
         <ButtonGrid>
-          <Button onClick={action('button clicked')}>DefaultDefaultDefaultDefaultDefaultDefault</Button>
-          <Button mode="primary" onClick={action('button clicked')}>PrimaryPrimaryPrimaryPrimaryPrimaryPrimary</Button>
-          <Button
-              mode="secondary"
-              onClick={action('button clicked')}>
-            SecondarySecondarySecondarySecondarySecondary
-          </Button>
+          <Button onClick={action('button clicked')}>TextTextTextTextTextTextTextTextTextText</Button>
+          <Button mode="primary" onClick={action('button clicked')}>TextTextTextTextTextTextTextTextTextText</Button>
+          <Button mode="secondary" onClick={action('button clicked')}>TextTextTextTextTextTextTextTextTextText</Button>
+          <Button mode="positive" onClick={action('button clicked')}>TextTextTextTextTextTextTextTextTextText</Button>
+          <Button mode="negative" onClick={action('button clicked')}>TextTextTextTextTextTextTextTextTextText</Button>
+          <Button mode="subtle" onClick={action('button clicked')}>TextTextTextTextTextTextTextTextTextText</Button>
         </ButtonGrid>
       </CardSegment>
     </Card>
