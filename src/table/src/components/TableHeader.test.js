25d33470911a8f9be7e98f1e1268b4919aa9b123
Solomon Tang <solomoncztang@gmail.com>
2019-09-06 19:17:50 -0700

    Add TableHeader Snapshots

--- src/table/src/components/TableHeader.test.js
+++ src/table/src/components/TableHeader.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 import TableHeader from './TableHeader';
 import { TABLE_HEADERS } from '../../stories/constants';
@@ -20,7 +21,11 @@ describe('TableHeader', () => {
     describe('onSort', () => {
       test('invoke onSort with property when HeadCell is clicked', () => {
         const mockOnSort = jest.fn();
-        const wrapper = shallow(<TableHeader onSort={mockOnSort} headers={TABLE_HEADERS} />);
+        const wrapper = shallow(
+          <TableHeader
+              headers={TABLE_HEADERS}
+              onSort={mockOnSort} />
+        );
 
         const headerCells = wrapper.find(HeadCell);
         expect(mockOnSort).toHaveBeenCalledTimes(0);
@@ -32,18 +37,25 @@ describe('TableHeader', () => {
         headerCells.get(1).props.onClick();
         expect(mockOnSort).toHaveBeenCalledTimes(2);
         expect(mockOnSort.mock.calls[1][1]).toEqual('dob');
+        expect(toJson(wrapper)).toMatchSnapshot();
       });
     });
 
     describe('order and orderBy', () => {
       test('should pass order to HeadCell when orderBy matches header key', () => {
-        const wrapper = shallow(<TableHeader headers={TABLE_HEADERS} order="asc" orderBy="name" />);
+        const wrapper = shallow(
+          <TableHeader
+              headers={TABLE_HEADERS}
+              order="asc"
+              orderBy="name" />
+        );
 
         const headerCells = wrapper.find(HeadCell);
         expect(headerCells.get(0).props.order).toEqual('asc');
         expect(headerCells.get(1).props.order).toEqual(false);
         expect(headerCells.get(2).props.order).toEqual(false);
         expect(headerCells.get(3).props.order).toEqual(false);
+        expect(toJson(wrapper)).toMatchSnapshot();
       });
     });
   });
@@ -52,6 +64,7 @@ describe('TableHeader', () => {
     test('should render a thead at the root', () => {
       const wrapper = shallow(<TableHeader />);
       expect(wrapper.type()).toEqual('thead');
+      expect(toJson(wrapper)).toMatchSnapshot();
     });
 
     test('should render sortable HeadCells', () => {
@@ -62,6 +75,7 @@ describe('TableHeader', () => {
         expect(headerCell.prop('sortable')).toEqual(true);
       });
       expect(headerCells).toHaveLength(TABLE_HEADERS.length);
+      expect(toJson(wrapper)).toMatchSnapshot();
     });
   });
 });
