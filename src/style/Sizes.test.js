ae5802624b5798140165abe18420ef89d8e47080
Hristo Oskov <hristo@openlattice.com>
2019-09-27 12:37:21 -0700

    tests for sizes

---
+++ src/style/Sizes.test.js
@@ -0,0 +1,21 @@
+import { Map } from 'immutable';
+
+import * as Sizes from './Sizes';
+
+const EXPECTED = Map({
+  APP_CONTAINER_MAX_WIDTH: 2000,
+  APP_CONTAINER_MIN_WIDTH: 1020,
+  APP_CONTENT_PADDING: 30,
+  APP_CONTENT_WIDTH: 960,
+});
+
+describe('Sizes', () => {
+
+  EXPECTED.forEach((value, key) => {
+    test(`should export "${key}: ${value}"`, () => {
+      expect(Sizes).toHaveProperty(key);
+      expect(Sizes[key]).toEqual(value);
+    });
+  });
+
+});
