f76e6f510755cea791dd8c5d3431d0de24a2c286
Solomon Tang <solomoncztang@gmail.com>
2019-10-23 16:10:13 -0700

    Remove unused imports

--- src/datetime/src/components/DatePicker.test.js
+++ src/datetime/src/components/DatePicker.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { act } from 'react-dom/test-utils';
 import { DateTime } from 'luxon';
 import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
