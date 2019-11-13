7edb3fff89c71e94b2b6ddc642af413681b89f87
Solomon Tang <solomoncztang@gmail.com>
2019-11-12 20:16:02 -0800

    Add media to StyleUtils

--- src/utils/StyleUtils.js
+++ src/utils/StyleUtils.js
@@ -48,8 +48,28 @@ const getHoverStyles = (props :{ onClick :() => any }) => {
   return null;
 };
 
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+  phone: 576,
+};
+
+// https://www.styled-components.com/docs/advanced#media-templates
+// font-size: 16
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)}
+    }
+  `;
+
+  return acc;
+}, {});
+
+
 export {
   getHoverStyles,
   getStickyPosition,
   getStyleVariation,
+  media,
 };
