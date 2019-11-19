9e8bf848343f688e32068977b2232d98bf5d5a9e
Giulia <giuliaecampana@gmail.com>
2019-11-18 17:53:45 -0800

    Badge story

---
+++ src/badge/stories/badge.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { Card, CardSegment } from '../../layout';
+import Badge from '..';
+
+storiesOf('Badge', module)
+  .add('default', () => (
+    <>
+      <h1>Badges</h1>
+      <Card>
+        <CardSegment>
+          <Badge>5</Badge>
+          <Badge mode="primary">25</Badge>
+          <Badge mode="secondary">10</Badge>
+          <Badge mode="added">+100</Badge>
+          <Badge mode="removed">-99</Badge>
+        </CardSegment>
+      </Card>
+    </>
+  ));
