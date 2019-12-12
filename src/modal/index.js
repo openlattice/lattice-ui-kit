4bffe2ceb1c18174464985dfb3c1b9ffec8e8551
Giulia <giuliaecampana@gmail.com>
2019-12-11 19:00:31 -0800

    Sort

--- src/modal/index.js
+++ src/modal/index.js
@@ -4,5 +4,5 @@
 
 export { default as ActionModal } from './src/components/ActionModal';
 export { default as Modal } from './src/components/Modal';
-export { default as ModalHeader } from './src/components/ModalHeader';
 export { default as ModalFooter } from './src/components/ModalFooter';
+export { default as ModalHeader } from './src/components/ModalHeader';
