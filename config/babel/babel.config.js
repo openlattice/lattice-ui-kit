a4b3c87f6584921bb7fa0de86ca377612ef40bbe
Drew <drew@openlattice.com>
2020-04-16 19:31:18 -0400

    I made it smol
    (cherry picked from commit e7a586739662d1c2edba95b8df582993ef2fdc0c)

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -3,7 +3,25 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-runtime',
-    'babel-plugin-styled-components'
+    'babel-plugin-styled-components',
+    ['babel-plugin-transform-imports', {
+      '@fortawesome/pro-duotone-svg-icons': {
+        transform: (importName, matches) => `@fortawesome/pro-duotone-svg-icons/${importName}`,
+        preventFullImport: true
+      },
+      '@fortawesome/pro-light-svg-icons': {
+        transform: (importName, matches) => `@fortawesome/pro-light-svg-icons/${importName}`,
+        preventFullImport: true
+      },
+      '@fortawesome/pro-regular-svg-icons': {
+        transform: (importName, matches) => `@fortawesome/pro-regular-svg-icons/${importName}`,
+        preventFullImport: true
+      },
+      '@fortawesome/pro-solid-svg-icons': {
+        transform: (importName, matches) => `@fortawesome/pro-solid-svg-icons/${importName}`,
+        preventFullImport: true
+      },
+    }]
   ],
   presets: [
     '@babel/preset-env',
