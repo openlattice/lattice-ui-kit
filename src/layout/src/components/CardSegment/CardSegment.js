85230d9856afeeb13958d3d41fa0bc81104ecfcb
Hristo Oskov <hristo@openlattice.com>
2020-05-19 17:46:36 -0700

    CardSegment noBorder prop ; fix overly complicated selectors for borders

--- src/layout/src/components/CardSegment/CardSegment.js
+++ src/layout/src/components/CardSegment/CardSegment.js
@@ -5,24 +5,25 @@
 import isFunction from 'lodash/isFunction';
 import styled, { css } from 'styled-components';
 
-import * as Colors from '../../../../colors';
-
-const { NEUTRALS } = Colors;
+import { NEUTRALS } from '../../../../colors';
 
 type ComputedSegmentProps = {
   bgColor ?:string;
   indent ?:number;
   noBleed ?:boolean;
+  noBorder ?:boolean;
   onClick ?:() => void;
   padding ?:string;
   vertical ?:boolean;
 };
 
 const getSegmentComputedStyles = (props :ComputedSegmentProps) => {
+
   const {
     bgColor,
     indent,
     noBleed,
+    noBorder,
     onClick,
     padding,
     vertical,
@@ -71,11 +72,18 @@ const getSegmentComputedStyles = (props :ComputedSegmentProps) => {
     flexDirection = 'column';
   }
 
+  let borderBottom;
+  if (!noBorder) {
+    borderBottom = `1px solid ${NEUTRALS[4]}`;
+  }
+
   const styles = css`
+    border-bottom: ${borderBottom};
     background-color: ${backgroundColor};
     flex-direction: ${flexDirection};
     margin: ${finalMargin};
     padding: ${finalPadding};
+
     &:hover {
       cursor: ${cursor};
     }
@@ -90,15 +98,11 @@ const CardSegment = styled.div`
   position: relative;
   ${getSegmentComputedStyles}
 
-  & & {
-    border-bottom: 1px solid ${NEUTRALS[4]};
-  }
-
-  & &:first-child {
+  &:first-child {
     border-radius: 3px 3px 0 0;
   }
 
-  & &:last-child {
+  &:last-child {
     border-bottom: 0;
     border-radius: 0 0 3px 3px;
   }
