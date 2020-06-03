798d37a90885cd4c91e703954073ac842aa0979f
Solomon Tang <solomoncztang@gmail.com>
2020-06-02 17:48:06 -0700

    Add V2 color families

--- src/colors/src/Colors.js
+++ src/colors/src/Colors.js
@@ -82,3 +82,100 @@ export const WHITE :string = '#ffffff';
  */
 
 export const OVERLAY_BG :string = rgba('#080019', 0.3);
+
+// V2 Color Families
+
+export const NEUTRAL = {
+  N00: '#F9F9FC',
+  N50: '#F2F2F4',
+  N100: '#DFDFE3',
+  N200: '#D1D1D6',
+  N300: '#C3C3C8',
+  N400: '#A7A7AC',
+  N500: '#8B8B90',
+  N600: '#717175',
+  N700: '#56565A',
+  N800: '#3C3C40',
+  N900: '#1F1F22',
+};
+
+export const PURPLE = {
+  P00: '#F2F0FF',
+  P100: '#CAC0FF',
+  P200: '#A291FF',
+  P300: '#7654FF',
+  P400: '#4F3FA7',
+  P500: '#261E52',
+};
+
+export const BLUE = {
+  B00: '#E9F2FF',
+  B100: '#BBD8FF',
+  B200: '#87BBFF',
+  B300: '#4F9AFF',
+  B400: '#2161B7',
+  B500: '#152C4A',
+};
+
+export const TEAL = {
+  T00: '#EBF3F3',
+  T100: '#ABE4E6',
+  T200: '#69D4D8',
+  T300: '#23C3C9',
+  T400: '#0D858A',
+  T500: '',
+};
+
+export const GREEN = {
+  G00: '#D9F3E8',
+  G100: '#9AE5B8',
+  G200: '#50CB89',
+  G300: '#00AF66',
+  G400: '#017546',
+  G500: '#003925',
+};
+
+export const YELLOW = {
+  Y00: '#FFF8DA',
+  Y100: '#FFEC93',
+  Y200: '#FFDF4C',
+  Y300: '#FFD100',
+  Y400: '#A98500',
+  Y500: '#342200',
+};
+
+export const ORANGE = {
+  O00: '#FFF2EC',
+  O100: '#FFD2B9',
+  O200: '#FFB083',
+  O300: '#FF7C33',
+  O400: '#A34A25',
+  O500: '#4A1F0C',
+};
+
+export const RED = {
+  R00: '#FFECEB',
+  R100: '#FABBB6',
+  R200: '#F4857E',
+  R300: '#ED4545',
+  R400: '#9C2D23',
+  R500: '#4D0600',
+};
+
+export const MAGENTA = {
+  M00: '#FFEBF3',
+  M100: '#FFC0DA',
+  M200: '#FE92BF',
+  M300: '#FC63A3',
+  M400: '#A93364',
+  M500: '#520022',
+};
+
+export const VIOLET = {
+  V00: '#FCEEFF',
+  V100: '#F2CAFC',
+  V200: '#E7A0F9',
+  V300: '#DA72F4',
+  V400: '#943FAA',
+  V500: '#3F004F',
+};
