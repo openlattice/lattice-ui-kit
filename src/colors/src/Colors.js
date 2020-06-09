de2673e357d53283959b7722c166132b5e9b553b
Solomon Tang <solomoncztang@gmail.com>
2020-06-08 22:11:00 -0700

    lower case hex values

--- src/colors/src/Colors.js
+++ src/colors/src/Colors.js
@@ -86,96 +86,96 @@ export const OVERLAY_BG :string = rgba('#080019', 0.3);
 // V2 Color Families
 
 export const NEUTRAL = {
-  N00: '#F9F9FC',
-  N50: '#F2F2F4',
-  N100: '#DFDFE3',
-  N200: '#D1D1D6',
-  N300: '#C3C3C8',
-  N400: '#A7A7AC',
-  N500: '#8B8B90',
+  N00: '#f9f9fc',
+  N50: '#f2f2f4',
+  N100: '#dfdfe3',
+  N200: '#d1d1d6',
+  N300: '#c3c3c8',
+  N400: '#a7a7ac',
+  N500: '#8b8b90',
   N600: '#717175',
-  N700: '#56565A',
-  N800: '#3C3C40',
-  N900: '#1F1F22',
+  N700: '#56565a',
+  N800: '#3c3c40',
+  N900: '#1f1f22',
 };
 
 export const PURPLE = {
-  P00: '#F2F0FF',
-  P100: '#CAC0FF',
-  P200: '#A291FF',
-  P300: '#7654FF',
-  P400: '#4F3FA7',
-  P500: '#261E52',
+  P00: '#f2f0ff',
+  P100: '#cac0ff',
+  P200: '#a291ff',
+  P300: '#7654ff',
+  P400: '#4f3fa7',
+  P500: '#261e52',
 };
 
 export const BLUE = {
-  B00: '#E9F2FF',
-  B100: '#BBD8FF',
-  B200: '#87BBFF',
-  B300: '#4F9AFF',
-  B400: '#2161B7',
-  B500: '#152C4A',
+  B00: '#e9f2ff',
+  B100: '#bbd8ff',
+  B200: '#87bbff',
+  B300: '#4f9aff',
+  B400: '#2161b7',
+  B500: '#152c4a',
 };
 
 export const TEAL = {
-  T00: '#EBF3F3',
-  T100: '#ABE4E6',
-  T200: '#69D4D8',
-  T300: '#23C3C9',
-  T400: '#0D858A',
-  T500: '#0C3C3E',
+  T00: '#ebf3f3',
+  T100: '#abe4e6',
+  T200: '#69d4d8',
+  T300: '#23c3c9',
+  T400: '#0d858a',
+  T500: '#0c3c3e',
 };
 
 export const GREEN = {
-  G00: '#D9F3E8',
-  G100: '#9AE5B8',
-  G200: '#50CB89',
-  G300: '#00AF66',
+  G00: '#d9f3e8',
+  G100: '#9ae5b8',
+  G200: '#50cb89',
+  G300: '#00af66',
   G400: '#017546',
   G500: '#003925',
 };
 
 export const YELLOW = {
-  Y00: '#FFF8DA',
-  Y100: '#FFEC93',
-  Y200: '#FFDF4C',
-  Y300: '#FFD100',
-  Y400: '#A98500',
+  Y00: '#fff8da',
+  Y100: '#ffec93',
+  Y200: '#ffdf4c',
+  Y300: '#ffd100',
+  Y400: '#a98500',
   Y500: '#342200',
 };
 
 export const ORANGE = {
-  O00: '#FFF2EC',
-  O100: '#FFD2B9',
-  O200: '#FFB083',
-  O300: '#FF7C33',
-  O400: '#A34A25',
-  O500: '#4A1F0C',
+  O00: '#fff2ec',
+  O100: '#ffd2b9',
+  O200: '#ffb083',
+  O300: '#ff7c33',
+  O400: '#a34a25',
+  O500: '#4a1f0c',
 };
 
 export const RED = {
-  R00: '#FFECEB',
-  R100: '#FABBB6',
-  R200: '#F4857E',
-  R300: '#ED4545',
-  R400: '#9C2D23',
-  R500: '#4D0600',
+  R00: '#ffeceb',
+  R100: '#fabbb6',
+  R200: '#f4857e',
+  R300: '#ed4545',
+  R400: '#9c2d23',
+  R500: '#4d0600',
 };
 
 export const MAGENTA = {
-  M00: '#FFEBF3',
-  M100: '#FFC0DA',
-  M200: '#FE92BF',
-  M300: '#FC63A3',
-  M400: '#A93364',
+  M00: '#ffebf3',
+  M100: '#ffc0da',
+  M200: '#fe92bf',
+  M300: '#fc63a3',
+  M400: '#a93364',
   M500: '#520022',
 };
 
 export const VIOLET = {
-  V00: '#FCEEFF',
-  V100: '#F2CAFC',
-  V200: '#E7A0F9',
-  V300: '#DA72F4',
-  V400: '#943FAA',
-  V500: '#3F004F',
+  V00: '#fceeff',
+  V100: '#f2cafc',
+  V200: '#e7a0f9',
+  V300: '#da72f4',
+  V400: '#943faa',
+  V500: '#3f004f',
 };
