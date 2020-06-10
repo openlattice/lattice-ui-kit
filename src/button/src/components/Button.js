063bff0e07f50ff6156131eea3f27dd48f99f3f1
Solomon Tang <solomoncztang@gmail.com>
2020-06-10 05:41:46 -0700

    avoid MUI throwing error for custom classes

--- src/button/src/components/Button.js
+++ src/button/src/components/Button.js
@@ -1,365 +1,96 @@
 // @flow
-import React from 'react';
+import React, { useMemo } from 'react';
 
+import clsx from 'clsx';
 import { Button as MuiButton } from '@material-ui/core';
-import { fade, withStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { mergeAll } from 'lodash/fp';
 import type { ButtonProps } from '@material-ui/core';
 
+import createColorStyles, { styleName } from './createColorStyles';
+
 import Spinner from '../../../spinner';
-import { NEUTRAL } from '../../../colors';
 
-export const styles = (theme :any) => ({
-  root: {},
-  /* Styles applied to the span element that wraps the children. */
-  label: {
-    fontWeight: 600,
-    lineHeight: 1.2,
-  },
-  /* Styles applied to the root element if `variant="text"`. */
-  text: {
-    padding: '0 16px',
-  },
-  /* Styles applied to the root element if `variant="text"` and `color="primary"`. */
-  textPrimary: {
-    color: theme.palette.primary.main,
-    '&:hover': {
-      backgroundColor: fade(theme.palette.primary.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="text"` and `color="secondary"`. */
-  textSecondary: {
-    color: theme.palette.secondary.main,
-    '&:hover': {
-      backgroundColor: fade(theme.palette.secondary.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="text"` and `color="positive"`. */
-  textPositive: {
-    color: theme.palette.success.main,
-    '&:hover': {
-      backgroundColor: fade(theme.palette.success.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="text"` and `color="negative"`. */
-  textNegative: {
-    color: theme.palette.error.main,
-    '&:hover': {
-      backgroundColor: fade(theme.palette.error.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="text"` and `color="warning"`. */
-  textWarning: {
-    color: theme.palette.warning.main,
-    '&:hover': {
-      backgroundColor: fade(theme.palette.warning.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="outlined"`. */
-  outlined: {
-    padding: '0 15px',
-    border: `1px solid ${
-      theme.palette.type === 'light' ? 'rgba(0, 0, 0, 0.23)' : 'rgba(255, 255, 255, 0.23)'
-    }`,
-    '&$disabled': {
-      border: `1px solid ${theme.palette.action.disabledBackground}`,
-    },
-  },
-  /* Styles applied to the root element if `variant="outlined"` and `color="primary"`. */
-  outlinedPrimary: {
-    color: theme.palette.primary.main,
-    border: `1px solid ${fade(theme.palette.primary.main, 0.5)}`,
-    '&:hover': {
-      border: `1px solid ${theme.palette.primary.main}`,
-      backgroundColor: fade(theme.palette.primary.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="outlined"` and `color="secondary"`. */
-  outlinedSecondary: {
-    color: theme.palette.secondary.main,
-    border: `1px solid ${fade(theme.palette.secondary.main, 0.5)}`,
-    '&:hover': {
-      border: `1px solid ${theme.palette.secondary.main}`,
-      backgroundColor: fade(theme.palette.secondary.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-    '&$disabled': {
-      border: `1px solid ${theme.palette.action.disabled}`,
-    },
-  },
-  /* Styles applied to the root element if `variant="outlined"` and `color="positive"`. */
-  outlinedPositive: {
-    color: theme.palette.success.main,
-    border: `1px solid ${fade(theme.palette.success.main, 0.5)}`,
-    '&:hover': {
-      border: `1px solid ${theme.palette.success.main}`,
-      backgroundColor: fade(theme.palette.success.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="outlined"` and `color="negative"`. */
-  outlinedNegative: {
-    color: theme.palette.error.main,
-    border: `1px solid ${fade(theme.palette.error.main, 0.5)}`,
-    '&:hover': {
-      border: `1px solid ${theme.palette.error.main}`,
-      backgroundColor: fade(theme.palette.error.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="outlined"` and `color="warning"`. */
-  outlinedWarning: {
-    color: theme.palette.warning.main,
-    border: `1px solid ${fade(theme.palette.warning.main, 0.5)}`,
-    '&:hover': {
-      border: `1px solid ${theme.palette.warning.main}`,
-      backgroundColor: fade(theme.palette.warning.main, theme.palette.action.hoverOpacity),
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: 'transparent',
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="contained"`. */
-  contained: {
-    padding: '0 16px',
-    backgroundColor: NEUTRAL.N50,
-    boxShadow: 'none',
-    color: NEUTRAL.N700,
-    '&:hover': {
-      backgroundColor: NEUTRAL.N100,
-      boxShadow: 'none',
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        boxShadow: 'none',
-        backgroundColor: NEUTRAL.N50,
-      },
-      '&$disabled': {
-        backgroundColor: theme.palette.action.disabledBackground,
-      },
-    },
-    '&$focusVisible': {
-      boxShadow: 'none',
-    },
-    '&:active': {
-      boxShadow: 'none',
-    },
-    '&$disabled': {
-      color: theme.palette.action.disabled,
-      boxShadow: 'none',
-      backgroundColor: theme.palette.action.disabledBackground,
-    },
-  },
-  /* Styles applied to the root element if `variant="contained"` and `color="primary"`. */
-  containedPrimary: {
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    '&:hover': {
-      backgroundColor: theme.palette.primary.dark,
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: theme.palette.primary.main,
-      },
-    },
-  },
-  /* Styles applied to the root element if `variant="contained"` and `color="secondary"`. */
-  containedSecondary: {
-    color: theme.palette.secondary.contrastText,
-    backgroundColor: theme.palette.secondary.main,
-    '&:hover': {
-      backgroundColor: theme.palette.secondary.dark,
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: theme.palette.secondary.main,
-      },
-    },
-  },
-  containedPositive: {
-    color: theme.palette.success.contrastText,
-    backgroundColor: theme.palette.success.main,
-    '&:hover': {
-      backgroundColor: theme.palette.success.dark,
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: theme.palette.success.main,
-      },
-    },
-  },
-  containedNegative: {
-    color: theme.palette.error.contrastText,
-    backgroundColor: theme.palette.error.main,
-    '&:hover': {
-      backgroundColor: theme.palette.error.dark,
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: theme.palette.error.main,
-      },
-    },
-  },
-  containedWarning: {
-    color: theme.palette.warning.contrastText,
-    backgroundColor: theme.palette.warning.main,
-    '&:hover': {
-      backgroundColor: theme.palette.warning.dark,
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: theme.palette.warning.main,
-      },
-    },
-  },
-  /* Pseudo-class applied to the ButtonBase root element if the button is keyboard focused. */
-  focusVisible: {},
-  /* Pseudo-class applied to the root element if `disabled={true}`. */
-  disabled: {},
-  /* Styles applied to the root element if `color="inherit"`. */
-  colorInherit: {
-    color: 'inherit',
-    borderColor: 'currentColor',
-  },
-  /* Styles applied to the root element if `size="small"` and `variant="text"`. */
-  textSizeSmall: {
-    padding: '5px',
-    fontSize: theme.typography.pxToRem(14),
-  },
-  /* Styles applied to the root element if `size="large"` and `variant="text"`. */
-  textSizeLarge: {
-    minHeight: '46px',
-    padding: '0 40px',
-    fontSize: theme.typography.pxToRem(18),
-  },
-  /* Styles applied to the root element if `size="small"` and `variant="outlined"`. */
-  outlinedSizeSmall: {
-    minHeight: '26px',
-    padding: '4px',
-    fontSize: theme.typography.pxToRem(14),
-  },
-  /* Styles applied to the root element if `size="large"` and `variant="outlined"`. */
-  outlinedSizeLarge: {
-    minHeight: '46px',
-    padding: '0 38px',
-    fontSize: theme.typography.pxToRem(18),
-  },
-  /* Styles applied to the root element if `size="small"` and `variant="contained"`. */
-  containedSizeSmall: {
-    minHeight: '26px',
-    padding: '5px',
-    fontSize: theme.typography.pxToRem(14),
-  },
-  /* Styles applied to the root element if `size="large"` and `variant="contained"`. */
-  containedSizeLarge: {
-    minHeight: '46px',
-    padding: '0 40px',
-    fontSize: theme.typography.pxToRem(18),
-  },
-  /* Styles applied to the root element if `size="small"`. */
-  sizeSmall: {
-    minWidth: '26px',
-  },
-  /* Styles applied to the root element if `size="large"`. */
-  sizeLarge: {
-    minWidth: '26px',
-  },
-  /* Styles applied to the root element if `fullWidth={true}`. */
-  fullWidth: {
-    width: '100%',
-  },
-  /* Styles applied to the startIcon element if supplied. */
-  startIcon: {
-    display: 'inherit',
-    marginRight: 8,
-    marginLeft: -4,
-    '&$iconSizeSmall': {
-      marginLeft: -2,
-    },
-  },
-  /* Styles applied to the endIcon element if supplied. */
-  endIcon: {
-    display: 'inherit',
-    marginRight: -4,
-    marginLeft: 8,
-    '&$iconSizeSmall': {
-      marginRight: -2,
-    },
-  },
-  /* Styles applied to the icon element if supplied and `size="small"`. */
-  iconSizeSmall: {
-    '& > *:first-child': {
-      fontSize: 'inherit',
-    },
-  },
-  /* Styles applied to the icon element if supplied and `size="medium"`. */
-  iconSizeMedium: {
-    '& > *:first-child': {
-      fontSize: 'inherit',
-    },
-  },
-  /* Styles applied to the icon element if supplied and `size="large"`. */
-  iconSizeLarge: {
-    '& > *:first-child': {
-      fontSize: 'inherit',
-    },
-  },
-});
+// avoid MUI errors from custom color classes
+// https://github.com/mui-org/material-ui/issues/13875#issuecomment-625358023
 
-const StyledButton = withStyles(styles, { name: 'Button' })(MuiButton);
+const variants = ['text', 'outlined', 'contained'];
+const colors = ['error', 'warning', 'info', 'success'];
+const isCustomColor = (color) => colors.includes(color);
 
-StyledButton.defaultProps = {
-  variant: 'contained',
-};
+const isOutlined = (variant) => variant === variants[1];
+const isContained = (variant) => variant === variants[2];
+
+const template = (variant, color, theme) => mergeAll([
+  {
+    color: theme.palette[color].main
+  },
+  isContained(variant) && {
+    color: theme.palette[color].contrastText,
+    backgroundColor: theme.palette[color].main
+  },
+  isOutlined(variant) && {
+    border: '1px solid',
+    borderColor: fade(theme.palette[color].main, 0.5)
+  },
+  {
+    '&:hover': mergeAll([
+      {
+        backgroundColor: fade(
+          theme.palette[color].main,
+          theme.palette.action.hoverOpacity
+        )
+      },
+      isContained(variant) && {
+        backgroundColor: theme.palette[color].dark
+      },
+      isOutlined(variant) && {
+        borderColor: theme.palette[color].main
+      },
+      {
+        // reset on touch devices
+        '@media (hover: none)': {
+          backgroundColor: 'transparent'
+        }
+      }
+    ])
+  }
+]);
+
+// curry global theme to create variant styles
+const useStyles = makeStyles(createColorStyles(variants, colors, template));
 
 const Button = (props :ButtonProps) => {
   const {
     children,
+    className,
+    color = 'default',
+    disableElevation = true,
     disabled,
+    fullWidth,
     isLoading,
     startIcon,
+    variant = 'contained',
     ...rest
   } = props;
+  const classes = useStyles();
+  const customColor = useMemo(() => isCustomColor(color), [color]);
   return (
-    <StyledButton
-        {...rest}
+    <MuiButton
+        className={clsx({
+          [className]: className,
+          [classes[styleName(variant, color)]]: customColor,
+        })}
+        color={customColor ? 'default' : color}
+        disableElevation={disableElevation}
+        variant={variant}
         disabled={(disabled || isLoading)}
-        startIcon={isLoading ? <Spinner /> : startIcon}>
+        startIcon={isLoading ? <Spinner /> : startIcon}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...rest}>
       { children }
-    </StyledButton>
+    </MuiButton>
   );
 };
 
 export default Button;
-
-// export type {
-//   ButtonMode,
-//   Props,
-// };
