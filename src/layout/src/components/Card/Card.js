6f3e461b2b27c1071dce4474ae90e8c1b07e6cf7
Solomon Tang <solomoncztang@gmail.com>
2020-07-06 23:00:21 -0700

    bring back hover styles for Card onClick

--- src/layout/src/components/Card/Card.js
+++ src/layout/src/components/Card/Card.js
@@ -2,11 +2,18 @@
 
 import React from 'react';
 
+import styled from 'styled-components';
 import { Card as MuiCard } from '@material-ui/core';
 import type { CardProps } from '@material-ui/core';
 
+import { getHoverStyles } from '../../../../utils/StyleUtils';
+
+const StyledMuiCard = styled(MuiCard)`
+  ${getHoverStyles}
+`;
+
 const Card = React.forwardRef<CardProps, MuiCard>((props, ref) => (
-  <MuiCard
+  <StyledMuiCard
       variant="outlined"
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
