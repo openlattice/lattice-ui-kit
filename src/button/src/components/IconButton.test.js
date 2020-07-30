7069b66272b8a28bfa72f32e823587503b19b9ea
Hristo Oskov <hristo@openlattice.com>
2020-07-30 14:30:42 -0700

    use a different icon in the test

--- src/button/src/components/IconButton.test.js
+++ src/button/src/components/IconButton.test.js
@@ -5,14 +5,14 @@
 import React from 'react';
 
 import toJson from 'enzyme-to-json';
-import { faSpaceShuttle } from '@fortawesome/pro-solid-svg-icons';
+import { faCode } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { mount } from 'enzyme';
 
 import IconButton from './IconButton';
 
-const LaunchIcon = (
-  <FontAwesomeIcon icon={faSpaceShuttle} rotate={-45} />
+const CodeIcon = (
+  <FontAwesomeIcon icon={faCode} />
 );
 
 describe('IconButton', () => {
@@ -20,20 +20,12 @@ describe('IconButton', () => {
   describe('snapshots', () => {
 
     test('default', () => {
-      const iconButton = mount(
-        <IconButton>
-          {LaunchIcon}
-        </IconButton>
-      );
+      const iconButton = mount(<IconButton>{CodeIcon}</IconButton>);
       expect(toJson(iconButton)).toMatchSnapshot();
     });
 
     test('spinner', () => {
-      const iconButton = mount(
-        <IconButton isLoading>
-          {LaunchIcon}
-        </IconButton>
-      );
+      const iconButton = mount(<IconButton isLoading>{CodeIcon}</IconButton>);
       expect(toJson(iconButton)).toMatchSnapshot();
     });
 
