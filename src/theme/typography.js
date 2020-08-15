4d37c9a560f498ef085eef5c35875561cef4c23a
Solomon Tang <solomoncztang@gmail.com>
2020-08-14 17:30:57 -0700

    adjust and expose typography

---
+++ src/theme/typography.js
@@ -0,0 +1,107 @@
+// override mui default
+// https://material-ui.com/customization/default-theme/
+
+const typography = {
+  htmlFontSize: 16,
+  fontFamily: "'Inter', 'Arial', sans-serif",
+  fontSize: 16,
+  fontWeightLight: 300,
+  fontWeightRegular: 400,
+  fontWeightMedium: 500,
+  fontWeightBold: 600,
+  h1: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 600,
+    fontSize: '1.75rem', // 28px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  h2: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 600,
+    fontSize: '1.5rem', // 24px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  h3: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 600,
+    fontSize: '1.375rem', // 22px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  h4: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.125rem', // 18px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  h5: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.125rem', // 18px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  h6: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1rem', // 16px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  subtitle1: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1rem', // 16px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  subtitle2: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 500,
+    fontSize: '1rem', // 16px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  body1: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 400,
+    fontSize: '1rem', // 16px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  body2: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 400,
+    fontSize: '1rem', // 16px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  caption: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontWeight: 400,
+    fontSize: '0.875rem', // 14px
+    lineHeight: 1.5,
+    letterSpacing: '0em',
+  },
+  button: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontSize: '1rem', // 16px
+    fontWeight: 400,
+    letterSpacing: '0em',
+    lineHeight: 1.5,
+    textTransform: 'none',
+  },
+  overline: {
+    fontFamily: "'Inter', 'Arial', sans-serif",
+    fontSize: '0.75rem', // 12px
+    fontWeight: 600,
+    letterSpacing: '0em',
+    lineHeight: 1.5,
+    textTransform: 'uppercase',
+  }
+};
+
+export default typography;
