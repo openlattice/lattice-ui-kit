f800117354cccca2ad4587ff46cd068257d4b15c
Hristo Oskov <hristo@openlattice.com>
2020-09-02 13:40:39 -0700

    allow children to be optional

--- src/modal/src/components/ModalBody.js
+++ src/modal/src/components/ModalBody.js
@@ -3,14 +3,15 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import type { Node } from 'react';
+
+import PropTypes from 'prop-types';
 
 import { BodySection } from './styled/StyledModalComponents';
 
 type Props = {
-  children :Node;
-  viewportScrolling ? :boolean;
+  children ?:Node;
+  viewportScrolling ?:boolean;
 };
 
 const ModalBody = (props :Props) => {
@@ -23,12 +24,13 @@ const ModalBody = (props :Props) => {
 };
 
 ModalBody.propTypes = {
-  children: PropTypes.node.isRequired,
-  viewportScrolling: PropTypes.bool
+  children: PropTypes.node,
+  viewportScrolling: PropTypes.bool,
 };
 
 ModalBody.defaultProps = {
-  viewportScrolling: false
+  children: null,
+  viewportScrolling: false,
 };
 
 export default ModalBody;
