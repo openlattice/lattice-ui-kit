302f6bfbb11e564d0fc9fa946b9419e62e8c9cd9
Todd Bergman <todd@openlattice.com>
2020-10-01 17:26:58 -0700

    Update styles for button to match designs

--- src/button/src/components/useButtonStyles.js
+++ src/button/src/components/useButtonStyles.js
@@ -3,26 +3,36 @@ import { mergeAll } from 'lodash/fp';
 
 // avoid MUI errors from custom color classes
 // https://github.com/mui-org/material-ui/issues/13875#issuecomment-625358023
+import { NEUTRAL } from '../../../colors';
 import createColorStyles from './createColorStyles';
 
 const variants = ['text', 'outlined', 'contained'];
-const colors = ['error', 'warning', 'info', 'success'];
+const colors = ['default', 'primary', 'secondary', 'error', 'warning', 'info', 'success'];
 export const isCustomColor = (color) => colors.includes(color);
 
 const isOutlined = (variant) => variant === variants[1];
 const isContained = (variant) => variant === variants[2];
+
+const getOutlineColor = (color, theme) => (color === colors[0] || color === colors[2])
+  ? theme.palette[color].contrastText : theme.palette[color].main;
 
 const template = (variant, color, theme) => mergeAll([
   {
     color: theme.palette[color].main
   },
   isContained(variant) && {
+    backgroundColor: theme.palette[color].main,
+    border: '1px solid transparent',
     color: theme.palette[color].contrastText,
-    backgroundColor: theme.palette[color].main
+    '&:focus': {
+        border: '1px solid white',
+        boxShadow: `0 0 0 2px ${getOutlineColor(color, theme)}`
+    },
   },
   isOutlined(variant) && {
     border: '1px solid',
-    borderColor: fade(theme.palette[color].main, 0.5)
+    borderColor: fade(getOutlineColor(color, theme), 0.5),
+    color: getOutlineColor(color, theme),
   },
   {
     '&:hover': mergeAll([
@@ -43,6 +53,13 @@ const template = (variant, color, theme) => mergeAll([
         '@media (hover: none)': {
           backgroundColor: 'transparent'
         }
+      }
+    ])
+  },
+  {
+    '&:active': mergeAll([
+      isContained(variant) && (theme.palette[color] && (theme.palette[color].contrastText !== 'white')) && {
+        color: 'white'
       }
     ])
   }
