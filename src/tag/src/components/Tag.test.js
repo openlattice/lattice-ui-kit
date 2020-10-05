1307baaef87868561e075beb6cc2557f97e76516
Todd Bergman <todd@openlattice.com>
2020-10-05 14:13:33 -0700

    updated tag tests

--- src/tag/src/components/Tag.test.js
+++ src/tag/src/components/Tag.test.js
@@ -8,11 +8,12 @@ import Tag from './Tag';
 import * as Colors from '../../../colors';
 
 const {
+  BLUE,
   GREEN,
   NEUTRAL,
   PURPLE,
-  RED,
-  YELLOW,
+  MAGENTA,
+  ORANGE,
 } = Colors;
 
 describe('Tag', () => {
@@ -35,38 +36,44 @@ describe('Tag', () => {
 
     test('danger', () => {
       const wrapper = mount(<Tag mode="danger">tag</Tag>);
-      expect(wrapper.find(Tag)).toHaveStyleRule('color', 'white');
-      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', RED.R300);
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', MAGENTA.M400);
+      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', MAGENTA.M00);
+    });
+
+    test('info', () => {
+      const wrapper = mount(<Tag mode="info">tag</Tag>);
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', BLUE.B400);
+      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', BLUE.B00);
     });
 
     test('neutral', () => {
       const wrapper = mount(<Tag mode="neutral">tag</Tag>);
-      expect(wrapper.find(Tag)).toHaveStyleRule('color', 'white');
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', NEUTRAL.N100);
       expect(wrapper.find(Tag)).toHaveStyleRule('background-color', NEUTRAL.N700);
     });
 
     test('primary', () => {
       const wrapper = mount(<Tag mode="primary">tag</Tag>);
-      expect(wrapper.find(Tag)).toHaveStyleRule('color', 'white');
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', PURPLE.P00);
       expect(wrapper.find(Tag)).toHaveStyleRule('background-color', PURPLE.P300);
     });
 
     test('secondary', () => {
       const wrapper = mount(<Tag mode="secondary">tag</Tag>);
-      expect(wrapper.find(Tag)).toHaveStyleRule('color', PURPLE.P300);
-      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', PURPLE.P100);
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', PURPLE.P400);
+      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', PURPLE.P00);
     });
 
     test('success', () => {
       const wrapper = mount(<Tag mode="success">tag</Tag>);
-      expect(wrapper.find(Tag)).toHaveStyleRule('color', 'white');
-      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', GREEN.G300);
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', GREEN.G400);
+      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', GREEN.G00);
     });
 
     test('warning', () => {
       const wrapper = mount(<Tag mode="warning">tag</Tag>);
-      expect(wrapper.find(Tag)).toHaveStyleRule('color', NEUTRAL.N700);
-      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', YELLOW.Y300);
+      expect(wrapper.find(Tag)).toHaveStyleRule('color', ORANGE.O400);
+      expect(wrapper.find(Tag)).toHaveStyleRule('background-color', ORANGE.O00);
     });
 
   });
