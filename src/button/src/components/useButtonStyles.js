2fcb22b48a25d87c351d25dd6d0f150dd864c561
Todd Bergman <todd@openlattice.com>
2020-10-09 20:17:41 -0700

    updated styles and adjustments to match designs

--- src/button/src/components/useButtonStyles.js
+++ src/button/src/components/useButtonStyles.js
@@ -3,50 +3,67 @@ import { mergeAll } from 'lodash/fp';
 
 // avoid MUI errors from custom color classes
 // https://github.com/mui-org/material-ui/issues/13875#issuecomment-625358023
-import { NEUTRAL } from '../../../colors';
+import {
+  BLUE,
+  GREEN,
+  NEUTRAL,
+  ORANGE,
+  PURPLE,
+  RED,
+} from '../../../colors';
 import createColorStyles from './createColorStyles';
 
+const outlinedHoverColors = {
+  error: RED.R00,
+  info: BLUE.B00,
+  primary: PURPLE.P100,
+  secondary: PURPLE.P00,
+  success: GREEN.G00,
+  warning: ORANGE.O00,
+};
+const outlinedActiveColors = {
+  error: RED.R100,
+  info: BLUE.B100,
+  primary: PURPLE.P200,
+  secondary: PURPLE.P100,
+  success: GREEN.G100,
+  warning: ORANGE.O100,
+};
 const variants = ['text', 'outlined', 'contained'];
-const colors = ['default', 'primary', 'secondary', 'error', 'warning', 'info', 'success'];
+const colors = ['primary', 'secondary', 'error', 'warning', 'info', 'success'];
 export const isCustomColor = (color) => colors.includes(color);
 
+const isText = (variant) => variant === variants[0];
 const isOutlined = (variant) => variant === variants[1];
 const isContained = (variant) => variant === variants[2];
 
-const getOutlineColor = (color, theme) => (color === colors[0] || color === colors[2])
-  ? theme.palette[color].contrastText : theme.palette[color].main;
+// Adjustments to colors that are outside of the palette's light, main, dark, & contrastText
+const getContrastColor = (color, theme) => ((color === colors[1])
+  ? (theme.palette[color].contrastText) : theme.palette[color].main);
+const getOutlinedHoverColor = (color) => outlinedHoverColors[color] || NEUTRAL.N050;
+const getOutlinedActiveColor = (color) => outlinedActiveColors[color] || NEUTRAL.N200;
 
-const template = (variant, color, theme) => mergeAll([
+const template = (variant, color, theme) => theme.palette[color] && mergeAll([
   {
-    color: theme.palette[color].main
+    color: getContrastColor(color, theme),
   },
   isContained(variant) && {
     backgroundColor: theme.palette[color].main,
     border: '1px solid transparent',
     color: theme.palette[color].contrastText,
-    '&:focus': {
-        border: '1px solid white',
-        boxShadow: `0 0 0 2px ${getOutlineColor(color, theme)}`
-    },
   },
   isOutlined(variant) && {
     border: '1px solid',
-    borderColor: fade(getOutlineColor(color, theme), 0.5),
-    color: getOutlineColor(color, theme),
+    borderColor: fade(getContrastColor(color, theme), 0.5),
   },
   {
     '&:hover': mergeAll([
-      {
-        backgroundColor: fade(
-          theme.palette[color].main,
-          theme.palette.action.hoverOpacity
-        )
-      },
       isContained(variant) && {
-        backgroundColor: theme.palette[color].dark
+        backgroundColor: theme.palette[color].light
       },
-      isOutlined(variant) && {
-        borderColor: theme.palette[color].main
+      (isOutlined(variant) || isText(variant)) && {
+        backgroundColor: getOutlinedHoverColor(color),
+        borderColor: 'transparent',
       },
       {
         // reset on touch devices
@@ -58,8 +75,27 @@ const template = (variant, color, theme) => mergeAll([
   },
   {
     '&:active': mergeAll([
+      {
+        boxShadow: 'none'
+      },
+      isContained(variant) && {
+        backgroundColor: theme.palette[color].dark
+      },
       isContained(variant) && (theme.palette[color] && (theme.palette[color].contrastText !== 'white')) && {
-        color: 'white'
+        color: 'white',
+      },
+      (isOutlined(variant) || isText(variant)) && {
+        backgroundColor: getOutlinedActiveColor(color),
+      }
+    ])
+  },
+  {
+    '&:focus': mergeAll([
+      {
+        boxShadow: `0 0 0 2px ${getContrastColor(color, theme)}`
+      },
+      isContained(variant) && {
+        border: `1px solid ${theme.palette.background.header}`,
       }
     ])
   }
