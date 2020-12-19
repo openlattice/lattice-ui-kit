860f074debcb6ed33c65f2b6e5f4e55a37de33a0
Solomon Tang <solomoncztang@gmail.com>
2020-12-18 20:21:32 -0800

    update exported chip

--- src/index.js
+++ src/index.js
@@ -141,6 +141,7 @@ export { default as Breadcrumbs } from './breadcrumbs';
 export { default as Skeleton } from './skeleton';
 export { default as Tooltip } from './tooltip';
 export { default as Switch } from './switch';
+export { default as Chip } from './chip';
 export { default as Typography } from './typography';
 export {
   ExpansionPanel,
@@ -153,7 +154,6 @@ export {
 } from './theme';
 
 export {
-  Chip,
   Collapse,
   Divider,
   Fab,
