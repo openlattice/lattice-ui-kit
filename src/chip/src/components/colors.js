98fa268e51a0b129cc412e2682c4d9e5c086ba71
Todd Bergman <todd@openlattice.com>
2021-03-22 14:22:38 -0700

    add deletable to colors

--- src/chip/src/components/colors.js
+++ src/chip/src/components/colors.js
@@ -21,54 +21,59 @@ export default {
   blue: {
     background: BLUE.B00,
     color: BLUE.B400,
-    clickable: BLUE.B100
+    clickable: BLUE.B100,
+    deletable: BLUE.B200,
   },
   green: {
     background: GREEN.G00,
     color: GREEN.G400,
-    clickable: GREEN.G100
+    clickable: GREEN.G100,
+    deletable: GREEN.G200,
   },
   magenta: {
     background: MAGENTA.M00,
     color: MAGENTA.M400,
-    clickable: MAGENTA.M100
+    clickable: MAGENTA.M100,
+    deletable: MAGENTA.M200,
   },
   orange: {
     background: ORANGE.O00,
     color: ORANGE.O500,
-    clickable: ORANGE.O100
-  },
-  purple: {
-    background: PURPLE.P00,
-    color: PURPLE.P400,
-    clickable: PURPLE.P100
+    clickable: ORANGE.O100,
+    deletable: ORANGE.O200,
   },
   red: {
     background: RED.R00,
     color: RED.R400,
-    clickable: RED.R100
+    clickable: RED.R100,
+    deletable: RED.R200,
   },
   teal: {
     background: TEAL.T00,
     color: TEAL.T500,
-    clickable: TEAL.T100
+    clickable: TEAL.T100,
+    deletable: TEAL.T200,
   },
   violet: {
     background: VIOLET.V00,
     color: VIOLET.V400,
-    clickable: VIOLET.V100
+    clickable: VIOLET.V100,
+    deletable: VIOLET.V200,
   },
   yellow: {
     background: YELLOW.Y00,
     color: YELLOW.Y500,
-    clickable: YELLOW.Y100
+    clickable: YELLOW.Y100,
+    deletable: YELLOW.Y200,
   },
   primary: {
     background: PURPLE.P300,
-    color: 'white'
+    color: 'white',
+    deletable: PURPLE.P200,
   },
   secondary: {
     background: PURPLE.P00,
-    color: PURPLE.P400
+    color: PURPLE.P400,
+    deletable: PURPLE.P200,
   },
 };
