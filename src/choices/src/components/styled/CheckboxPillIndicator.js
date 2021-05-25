69ab73744f29993ac4eed6b1aa8d8ce63ad50b83
Todd Bergman <todd@openlattice.com>
2021-05-25 16:38:35 -0400

    separate indicator for button due to same issue we were having with radio

---
+++ src/choices/src/components/styled/CheckboxPillIndicator.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import choicePillStyles from './ChoicePillStyles';
+
+import { duration } from '../../../../style/transitions';
+
+const CheckboxPillIndicator = styled.span`
+  transition: background-color ${duration.swift} ease-out,
+    border-color ${duration.swift} ease-out,
+    box-shadow ${duration.swift} ease-out,
+    color ${duration.swift} ease-out;
+
+  ${choicePillStyles};
+`;
+
+export default CheckboxPillIndicator;
