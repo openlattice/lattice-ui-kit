134063e61cfc176803381c667a3ba25812aeb43d
Solomon Tang <solomoncztang@gmail.com>
2021-06-11 10:42:17 -0700

    update depdendencies for webpack 5

--- config/babel/babel.config.js
+++ config/babel/babel.config.js
@@ -2,36 +2,46 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-runtime',
+    ['@babel/plugin-transform-runtime', {
+      corejs: 3,
+    }],
     [
       'babel-plugin-styled-components',
       { ssr: false, displayName: false, namespace: 'sc' },
     ],
     ['babel-plugin-transform-imports', {
-      '@fortawesome/pro-duotone-svg-icons': {
-        transform: (importName) => `@fortawesome/pro-duotone-svg-icons/${importName}`,
+      '@fortawesome/free-solid-svg-icons': {
+        transform: (importName) => `@fortawesome/free-solid-svg-icons/${importName}`,
         preventFullImport: true,
         skipDefaultConversion: true,
+      },
+      '@fortawesome/pro-duotone-svg-icons': {
+        preventFullImport: true,
+        skipDefaultConversion: true,
+        transform: (importName) => `@fortawesome/pro-duotone-svg-icons/${importName}`,
       },
       '@fortawesome/pro-light-svg-icons': {
-        transform: (importName) => `@fortawesome/pro-light-svg-icons/${importName}`,
         preventFullImport: true,
         skipDefaultConversion: true,
+        transform: (importName) => `@fortawesome/pro-light-svg-icons/${importName}`,
       },
       '@fortawesome/pro-regular-svg-icons': {
-        transform: (importName) => `@fortawesome/pro-regular-svg-icons/${importName}`,
         preventFullImport: true,
         skipDefaultConversion: true,
+        transform: (importName) => `@fortawesome/pro-regular-svg-icons/${importName}`,
       },
       '@fortawesome/pro-solid-svg-icons': {
-        transform: (importName) => `@fortawesome/pro-solid-svg-icons/${importName}`,
         preventFullImport: true,
         skipDefaultConversion: true,
+        transform: (importName) => `@fortawesome/pro-solid-svg-icons/${importName}`,
       },
     }],
   ],
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', {
+      corejs: '3',
+      useBuiltIns: 'entry',
+    }],
     '@babel/preset-flow',
     ['@babel/preset-react', {
       runtime: 'automatic'
--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -28,9 +28,6 @@ module.exports = {
   setupFiles: [
     '<rootDir>/config/jest/enzyme.config.js',
   ],
-  setupFilesAfterEnv: [
-    '<rootDir>/config/jest/test.config.js',
-  ],
   testEnvironment: '<rootDir>/config/jest/jsdom.config.js',
   testURL: 'http://localhost',
   transform: {
