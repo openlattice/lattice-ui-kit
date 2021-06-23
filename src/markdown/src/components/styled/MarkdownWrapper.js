e4c159a08a106f39a2f9895f12c7b5119cc9ab35
Solomon Tang <solomoncztang@gmail.com>
2021-06-22 18:02:46 -0700

    style lint

--- src/markdown/src/components/styled/MarkdownWrapper.js
+++ src/markdown/src/components/styled/MarkdownWrapper.js
@@ -8,29 +8,24 @@ const MarkdownWrapper = styled.div`
     background-color: rgba(27,31,35,.05);
     border-radius: 3px;
   }
-
-  .hljs{
+  .hljs {
     display: block;
     overflow-x: auto;
     padding: .5em;
     color: #333;
     background: #f8f8f8;
   }
-
   .hljs-comment, .hljs-quote {
     color: #998;
     font-style: italic;
   }
-
   .hljs-keyword, .hljs-selector-tag, .hljs-subst {
     color: #333;
     font-weight: 700
   }
-
-  .hljs-literal, .hljs-number, .hljs-tag .hljs-attr, .hljs-template-variable, .hljs-variable{
+  .hljs-literal, .hljs-number, .hljs-tag .hljs-attr, .hljs-template-variable, .hljs-variable {
     color: teal;
   }
-
   .hljs-doctag, .hljs-string {
     color: #d14;
   }
@@ -45,45 +40,35 @@ const MarkdownWrapper = styled.div`
     color: #458;
     font-weight: 700;
   }
-
   .hljs-attribute, .hljs-name, .hljs-tag {
     color: navy;
     font-weight: 400;
   }
-
   .hljs-link, .hljs-regexp {
     color: #009926;
   }
-
   .hljs-bullet, .hljs-symbol {
     color: #990073;
   }
-
   .hljs-built_in, .hljs-builtin-name {
     color: #0086b3;
   }
-
   .hljs-meta{
     color: #999;
     font-weight: 700;
   }
-
   .hljs-deletion {
     background: #fdd;
   }
-
   .hljs-addition{
     background: #dfd;
   }
-
   .hljs-emphasis {
     font-style: italic;
   }
-
   .hljs-strong{
     font-weight: 700;
   }
-
   table {
     display: block;
     width: 100%;
@@ -91,26 +76,21 @@ const MarkdownWrapper = styled.div`
     border-spacing: 0;
     border-collapse: collapse;
   }
-
   table th {
     font-weight: 600;
   }
-
   table td,
   table th {
     padding: 6px 13px;
     border: 1px solid #dfe2e5;
   }
-
   table tr {
     background-color: #fff;
     border-top: 1px solid #c6cbd1;
   }
-
   table tr:nth-child(2n) {
     background-color: #f6f8fa;
   }
-
 `;
 
 export {
